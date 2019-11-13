a9115c46972f2ba7ded981d7046d114135be4f96
cliffckerr <questionably@gmail.com>
2019-11-13 10:17:33 -0800

    working on simplifying run

--- commission_pyemod/commission_pyemod.py
+++ commission_pyemod/commission_pyemod.py
@@ -1,75 +1,41 @@
-import random
+import os
+import numpy as np
+import pyemod as em
 
-from GenerateCampaignRCM import *
-from idmtools.builders import ExperimentBuilder
-from idmtools.core.platform_factory import Platform
-from idmtools.managers import ExperimentManager
-from idmtools_model_emod.emod_experiment import EMODExperiment
+# This should be emod_api
+import json
+import GenerateCampaignRCM as gencam
 
 exp_name = 'Family Planning Workflow Development'
-config_fn = os.path.join('inputs', 'fp_default_config.json')
+inputs = 'inputs'
+config_file = os.path.join(inputs, 'config.json') # TODO: Remove boilerplate
+demographics_file = os.path.join(inputs, 'demographics.json')
 
 # Commonly modified calibration variables and configuration
-BASE_POPULATION_SCALE_FACTOR = 0.00333333333333333  # For quick test simulations, this is set to a very low value
+BASE_POPULATION_SCALE_FACTOR = 0.0033  # For quick test simulations, this is set to a very low value
 N_SAMPLES = 3  # the number of distinct parameter sets to run per iteration
 N_REPLICATES = 1  # replicates, 1 is highly recommended.
 
-samples = pd.DataFrame({'PillEfficacy': np.linspace(0, 1, N_SAMPLES)})
+samples = np.linspace(0, 1, N_SAMPLES)
 
-static_params = {
-    'Base_Population_Scale_Factor': BASE_POPULATION_SCALE_FACTOR,
-    'Custom_Individual_Events': [
-        'Should_Not_Be_Broadcasted',
-        'Choose_Next_Method_Currently_Under_Age',
-        'Choose_Next_Method_Currently_On_Implant',
-        'Choose_Next_Method_Currently_On_Pill',
-        'Choose_Next_Method_Currently_On_Withdrawal',
-        'Choose_Next_Method_Currently_Pregnant',
-        'Choose_Next_Method_Currently_Post_Partum',
-        'Choose_Next_Method_Currently_On_None',
-        'Use_Implant',
-        'Use_Pill',
-        'Use_Withdrawal',
-        'Use_None',
-    ],
-    'Enable_Property_Output': 1,
-    'Report_Event_Recorder': 1,
-    'Report_Event_Recorder_Events': [
-        'Pregnant',
-        'GaveBirth',
-        'Choose_Next_Method_Currently_Under_Age',
-        'Choose_Next_Method_Currently_On_Implant',
-        'Choose_Next_Method_Currently_On_Pill',
-        'Choose_Next_Method_Currently_On_Withdrawal',
-        'Choose_Next_Method_Currently_Pregnant',
-        'Choose_Next_Method_Currently_Post_Partum',
-        'Choose_Next_Method_Currently_On_None',
-        'Use_Implant',
-        'Use_Pill',
-        'Use_Withdrawal',
-        'Use_None',
-    ],
-    'Report_Event_Recorder_Ignore_Events_In_List': 0,
-    'Report_Event_Recorder_Individual_Properties': [
-        'CurrentStatus'
-    ],
-    'logLevel_RandomChoiceMatrix': 'ERROR'
-}
+base_sim= em.Simulation(config=config_file, demographics=demographics_file)
+
+def make_campaign(pill_efficacy):
+    campaign = em.default_campaign()
+    con_list = gencam.CreateContraceptives()
+    pill_contraceptive = next(x[1] for x in con_list if x[0] == gencam.USE_PILL)
+    pill_contraceptive.Waning_Config.Initial_Effect = pill_efficacy
+    rc_list = gencam.CreateRandomChoiceMatrixList()
+    campaign = gencam.GenerateCampaignFP(con_list, rc_list)
+    campaign.pars = json.loads(campaign.to_json())
+    return campaign
+
+for value in samples:
+    base_sim.campaign = make_campaign(pill_efficacy=value)
 
 
-def map_sample_to_model_input_fn(simulation, sample_dict):
-    con_list = CreateContraceptives()
 
-    pill_contraceptive = next(x[1] for x in con_list if x[0] == USE_PILL)
-    pill_contraceptive.Waning_Config.Initial_Effect = sample_dict['PillEfficacy']
 
-    rc_list = CreateRandomChoiceMatrixList()
-    campaign = GenerateCampaignFP(con_list, rc_list)
-
-    simulation.campaign = json.loads(campaign.to_json())
-
-    sample_index = sample_dict.pop("sample_index")
-    return {"sample_index": sample_index, **{'[SAMPLE] %s' % k: v for k, v in sample_dict.items()}}
 
 
 def change_seed(simulation, seed):
@@ -79,7 +45,7 @@ def change_seed(simulation, seed):
 
 if __name__ == "__main__":
     # Choose where to run
-    platform = Platform("COMPS2")
+    platform = Platform("COMPS")
 
     # Prepare the path for the exe and demographics needed
     eradication_path = os.path.join("bin", "Eradication_FPOngoing_a4be1893d72d4df3de568217ecc63ecfcaee17cf.exe")
