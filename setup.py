42b2ee3edcc73c0a6d7b6774b69eebe3d81116a5
cliffckerr <questionably@gmail.com>
2019-11-16 17:10:54 -0800

    updated readme

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 import os
 from setuptools import setup, find_packages
 
-# Get version
-versionfile = '__init__.py'
-versionstr = '__version__ = '
-cwd = os.path.abspath(os.path.dirname(__file__))
-versionpath = os.path.join(cwd, 'pyemod', versionfile)
-with open(versionpath, 'r') as f:
-    lines = f.readlines()
-    assert lines[0].startswith(versionstr)
-    version = lines[0].split(versionstr)[1].rstrip()
-
 CLASSIFIERS = [
     "Environment :: Console",
     "Intended Audience :: Science/Research",
@@ -23,11 +13,11 @@ CLASSIFIERS = [
 ]
 
 setup(
-    name="pyemod",
-    version=version,
-    author="Cliff Kerr, Benoit Raybaud, Jonathan Bloedow, Clinton Collins, Chris Lorton, Daniel Klein",
+    name="fp_analyses",
+    version='0.1',
+    author="Cliff Kerr, Daniel Klein",
     author_email="ckerr@idmod.org",
-    description="Python tools for using EMOD",
+    description="FP analyses",
     keywords=["EMOD"],
     platforms=["OS Independent"],
     classifiers=CLASSIFIERS,
@@ -37,6 +27,7 @@ setup(
         'numpy',
         'scipy',
         'matplotlib',
+        'pandas',
         'sciris',
     ],
 )
