425a2c15295c2d4f7adf9e53f26cd63c61f72db1
cliffckerr <questionably@gmail.com>
2019-11-17 00:15:37 -0800

    argh, HD5 file gets modified on load!!

--- fp_data/gdr_review/compare-dhs-uhri.py
+++ fp_data/gdr_review/compare-dhs-uhri.py
@@ -6,13 +6,13 @@ import fp_data.DHS.calobj as co
 
 sc.heading('Setting parameters...')
 
-uhri_file = os.path.join(os.pardir, 'UHRI', 'store.hdf')
+uhri_file = os.path.join(os.pardir, 'UHRI', 'senegal_women.obj')
 dhs_file = os.path.join(os.pardir, 'DHS', 'senegal.cal')
 
 
 #%% Loading data
 sc.heading('Loading data...')
-uhri = pd.HDFStore(uhri_file)['women']
+uhri = sc.loadobj(uhri_file)
 dhs = co.CalObj(dhs_file) # Create from saved data
 
 
