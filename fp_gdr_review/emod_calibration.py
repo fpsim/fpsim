1e6ee522dfa89e5b4f1a7f3c01704514f0f4d076
cliffckerr <questionably@gmail.com>
2019-11-18 01:22:41 -0800

    running, just have to put in real objective function

--- fp_gdr_review/emod_calibration.py
+++ fp_gdr_review/emod_calibration.py
@@ -19,37 +19,35 @@ sc.heading('Setting up simulations...')
 
 
 # Commonly modified calibration variables and configuration
-n_replicates = 1  # replicates, 1 is highly recommended.
+n_replicates = 10  # replicates, 1 is highly recommended.
 
 base_sim = em.Simulation(config=config_file, demographics=demographics_file)
 base_sim.demographics.update(pars=overlay_file) # TODO: make this simpler, or avoid it altogether
-#base_sim.demographics['Defaults']['IndividualAttributes']['FertilityDistribution']['ResultScaleFactor'] *= 5
 
-def make_campaign(use_contraception=True):
-    campaign = em.make_campaign()
-    if use_contraception:
-        con_list = gencam.CreateContraceptives()
-        rc_list = gencam.CreateRandomChoiceMatrixList()
-        campaign.pars = gencam.GenerateCampaignFP(con_list, rc_list)
-    return campaign
-
-sims = []
-for use_contraception in [False, True]:
-    for replicate in range(n_replicates):
-        sim = base_sim.copy()
-        sim.campaign = make_campaign(use_contraception)
-        sims.append(sim)
-
+#def make_campaign(use_contraception=True):
+#    campaign = em.make_campaign()
+#    if use_contraception:
+#        con_list = gencam.CreateContraceptives()
+#        rc_list = gencam.CreateRandomChoiceMatrixList()
+#        campaign.pars = gencam.GenerateCampaignFP(con_list, rc_list)
+#    return campaign
+#
+#sims = []
+#for use_contraception in [False, True]:
+#    for replicate in range(n_replicates):
+#        sim = base_sim.copy()
+#        sim.campaign = make_campaign(use_contraception)
+#        sims.append(sim)
 
 parameters = [{'name': 'fertility_scale_factor',
               'best': 2.0e-6,
               'min': 0.5e-6,
               'max': 10.0e-6}]
 
-
-def run_fn(x):
-    sim = base_sim.copy()
-    sim.demographics['Defaults']['IndividualAttributes']['FertilityDistribution']['ResultScaleFactor']
+def run_fn(x, sim):
+    print('hi!')
+    print(sim)
+    sim.demographics['Defaults']['IndividualAttributes']['FertilityDistribution']['ResultScaleFactor'] = x[0]
 #    for v,val in enumerate(x):
 #        par_name = parameters[v]['name']
 #        sim.config['parameters'][par_name] = val
@@ -57,9 +55,9 @@ def run_fn(x):
     error = pl.rand() # objective_fn(sim)
     return error    
 
-sim = sims[1]
+sim = base_sim.copy()
 calib = em.Calibration(sim=sim, parameters=parameters, run_fn=run_fn, algorithm=algorithm)
-results = calib.run()
+results = calib.run(args={'sim':calib.sim})
 
 sc.toc()
 print('Done.')
