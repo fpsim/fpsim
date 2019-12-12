got age-dependent mortality implemented, but wrong results

fp_lemod/senegal.py
@@ -27,11 +27,40 @@ if do_run:
     sim = lfp.Sim()
     sim.run()
     if do_plot:
+        
+        # Default plots
         fig = sim.plot(dosave=do_save)
-        ax = fig.axes[-1] # Population size plot
+        
+        # Population size plot
+        ax = fig.axes[-1] 
         ax.scatter(years, popsize, c='k', label='Data')
         pl.legend()
+    
+        # Age pyrmaid
+        fig2 = pl.figure(figsize=(20,14))
         
-
+        M = pop_pyr_2015.loc[:,'M'].to_numpy()
+        F = pop_pyr_2015.loc[:,'F'].to_numpy()
+        M = M/M.sum()
+        F = F/F.sum()
+        bins = 5*pl.arange(len(M))
+        
+        plotstyle = {'marker':'o', 'lw':2}
+        
+        pl.plot(bins, M, c='b', label='Males', **plotstyle)
+        pl.plot(bins, F, c='r', label='Females', **plotstyle)
+        
+        counts = pl.zeros(len(bins))
+        people = sim.people.values()
+        for person in people:
+            binind = sc.findinds(bins<=person.age)[-1]
+            counts[binind] += 1
+        counts = counts/counts.sum()
+        pl.plot(bins, counts, c='g', label='Model', **plotstyle)
+        pl.legend()
+        pl.xlabel('Age')
+        pl.ylabel('Proportion')
+            
+    
 print('Done.')
 