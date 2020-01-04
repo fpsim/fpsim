0be5d538fdcef255708e66d975577213d5c3aeb1
Daniel J. Klein <dklein@idmod.org>
2020-01-03 17:12:13 -0800

    Very close now with switching matrix, just off by small numbers
    Former-commit-id: 4a057913c54f377494358859304f2579bb1f452f

--- fp_data/URHI/analyze_calendar.py
+++ fp_data/URHI/analyze_calendar.py
@@ -34,38 +34,6 @@ try:
 except:
     raise Exception(f'User {username} not found among users {list(folderdict.keys())}, cannot find data folder')
 
-'''
-cipher_method = {
-    'B': 'Birth',
-    'T': 'Terminated pregnancy/non-live birth',
-    'P': 'Pregnancy',
-    '0': 'Non-use of contraception',
-    '1': 'Pill',
-    '2': 'IUD',
-    '3': 'Injectables',
-    '4': 'Diaphragm',
-    '5': 'Condom',
-    '6': 'Female sterilization',
-    '7': 'Male sterilization',
-    '8': 'Periodic abstinence/rhythm',
-    '9': 'Withdrawal',
-    'W': 'Other traditional methods',
-    'N': 'Implants', # Guessing this might instead be pregnancy?
-#   'G': 'Pregnant', # <-- Total guess, see 'G' 8x after N (Implants???) and 2x after each F (Foam and Jelly)
-    'A': 'Abstinence',
-    'L': 'Lactational amenorrhea method (LAM)',
-    'C': 'Female cggondom',
-    'F': 'Foam and Jelly',
-    'E': 'Emergency contraception (DHSVI)',
-    'S': 'Standard days method (DHSVI)',
-    'M': 'Other modern method (DHSVI)',
-    'α2': 'Country-specific method 1',
-    'ß2': 'Country-specific method 2',
-    'τ2': 'Country-specific method 3',
-    '?': 'Unknown method/missing data',
-}
-'''
-
 cipher_method = {
     'N': 'Birth',
     'G': 'Pregnancy',
@@ -73,7 +41,7 @@ cipher_method = {
     'M': 'Stillbirth',
     'A': 'Abortion',
 
-    '0': 'No Method',
+    '0': 'No method',
     '1': 'Female sterilization',
     '2': 'Male sterilization',
     '3': 'Implant',
@@ -84,7 +52,7 @@ cipher_method = {
     '8': 'Male condom',
     '9': 'Female condom',
 
-    'H': 'Menstrual cycle collar',
+    'H': 'Standard days method',
     'L': 'Lactational amenorrhea method (LAM)',
     'X': 'Other modern method',
 
@@ -117,13 +85,14 @@ cipher_marriage = {
 }
 
 def decrypt(dat):
-    method  = dat['cal_1']
-    source  = dat['cal_2']
-    reason  = dat['cal_3']
-    married = dat['cal_4']
-    other1   = dat['cal_1o']
-    other2   = dat['cal_2o']
-    other3   = dat['cal_3o']
+    # Note reverse because data starts in 2015 and goes backwards
+    method  = dat['cal_1'][::-1]
+    source  = dat['cal_2'][::-1]
+    reason  = dat['cal_3'][::-1]
+    married = dat['cal_4'][::-1]
+    other1   = dat['cal_1o'][::-1]
+    other2   = dat['cal_2o'][::-1]
+    other3   = dat['cal_3o'][::-1]
 
     print('-'*80)
     print('UID:', dat['UID'])
@@ -162,7 +131,8 @@ def decrypt(dat):
 
 
 if (not force_read) and os.path.isfile(cachefn) and 'cal' in store:
-    data = store['cal']
+    cal = store['cal']
+    data = store['all']
 else:
 #filenames = glob.glob( os.path.join(basedir, 'Endline', '*.dta'))
     filename = os.path.join( basedir, 'Endline', 'SEN_end_wm_match_20160505.dta' )
@@ -172,7 +142,7 @@ else:
     print(f'File: {filename} ...')
     data = pd.read_stata(filename, convert_categoricals=False)
 
-    if False:
+    if True:
         values = pd.io.stata.StataReader(filename).value_labels()
         codebook = pd.io.stata.StataReader(filename).variable_labels()
 
@@ -190,12 +160,51 @@ else:
     cal_3o	Column 3 of calendar - Other reason for discontinuation specified
     '''
 
+    store['all'] = data
+
     #print( '\n'.join(data.columns) )
-    store['cal'] = data[['UID', 'cal_1', 'cal_2', 'cal_3', 'cal_4', 'cal_1o', 'cal_2o', 'cal_3o', 'ewoman_weight']]
+    cal = data[['UID', 'cal_1', 'cal_2', 'cal_3', 'cal_4', 'cal_1o', 'cal_2o', 'cal_3o', 'ewoman_weight_6city']]
+    store['cal'] = cal
 
 store.close()
 
 print(f'Data has {data.shape[0]} rows.')
+
+# Check age distribution
+data['age_bin'] = pd.cut(data['ew102'], [15, 20, 25, 30, 35, 40, 55], right=False)
+age_summary = data.groupby('age_bin')['ewoman_weight_6city'].sum()
+print(100 * age_summary / age_summary.sum())
+
+# Check pregnancies
+def count_starts(x, code='G'):
+    if 'G' not in x:
+        return 0
+
+    p,q = itertools.tee(x.strip()[::-1])
+    p = list(p)[:-1]
+    next(q, None)
+
+    count = 1 if p[0] == code else 0
+    for a,b in zip(p,q):
+        if a != code and b == code:
+            count += 1
+
+    return count
+
+from functools import partial
+cal['Pregnancies'] = cal['cal_1'].apply(partial(count_starts, code='G'))
+print(f'Data has {cal["Pregnancies"].sum()} pregnancies.')
+
+cal['Failures'] = cal['cal_1'].apply(lambda x: len([c for c in x if c in ['A', 'F', 'M']]))
+print(f'Data has {cal["Failures"].sum()} abortions, miscarriages, and stillbirths.')
+
+cal['Births'] = cal['cal_1'].apply(lambda x: len([c for c in x if c == 'N']))
+print(f'Data has {cal["Births"].sum()} births.')
+
+cal['Non-Use'] = cal['cal_1'].apply(partial(count_starts, code='0'))
+print(f'Data has {cal["Non-Use"].sum()} continuous periods of non-use.')
+#with pd.option_context('display.max_colwidth', -1):
+#    print(cal[['cal_1', 'Non-Use']])
 
 chr_to_idx = {}
 def build_chr_to_idx(dat):
@@ -204,58 +213,92 @@ def build_chr_to_idx(dat):
             chr_to_idx[c] = len(chr_to_idx)
 
 def build_switching_matrix(dat):
-    inds = [chr_to_idx[c] for c in dat['cal_1'].strip() ]
+    # Note reverse because data starts in 2015 and goes backwards
+    inds = [chr_to_idx[c] for c in dat['cal_1'].strip()[::-1] ]
 
-    row, col = itertools.tee(inds)
-    next(col, None)
+    method_from, method_to = itertools.tee(inds)
+    next(method_to, None)
 
-    rows = np.array(list(row)[:-1])
-    cols = np.array(list(col))
-    vals = np.ones_like(rows)
-    vals_weighted = dat['ewoman_weight'] * np.ones_like(rows)
+    method_from = np.array(list(method_from)[:-1])
+    method_to = np.array(list(method_to))
 
-    #return coo_matrix((vals_weighted, (row, col)), shape=(row.max()+1, col.max()+1))
-    return rows, cols, vals, vals_weighted
+    vals = np.ones_like(method_from)
+    vals_weighted = dat['ewoman_weight_6city'] * np.ones_like(method_from)
+
+    return method_from, method_to, vals, vals_weighted
 
 
-data.apply(build_chr_to_idx, axis=1)
+cal.apply(build_chr_to_idx, axis=1)
 
-rows = []
-cols = []
+method_from = [] # rows
+method_to = [] # cols
 vals = []
 vals_weighted = []
 
-for idx, dat in data.iterrows():
+for idx, dat in cal.iterrows():
     r, c, v, vw = build_switching_matrix(dat)
-    rows.append(r)
-    cols.append(c)
+    method_from.append(r)
+    method_to.append(c)
     vals.append(v)
     vals_weighted.append(vw)
 
 
 index = [cipher_method[k] for k in chr_to_idx.keys()]
 
-rows = np.concatenate(rows)
-cols = np.concatenate(cols)
+method_from = np.concatenate(method_from)
+method_to = np.concatenate(method_to)
 vals_weighted = np.concatenate(vals_weighted)
-def form_switching_matrix(rows,  cols, vals, fn = None):
-    M = coo_matrix((vals_weighted, (rows, cols)), shape=(rows.max()+1, cols.max()+1)).todense()
+def form_switching_matrix(method_from,  method_to, vals, fn = None):
+    # method_from on rows, method_to on columns
+    M = coo_matrix((vals_weighted, (method_from, method_to)), shape=(method_from.max()+1, method_to.max()+1)).todense()
     M = pd.DataFrame(M, index = index, columns = index)
     if fn is not None:
         M.to_csv(fn)
 
     return M
 
-M = form_switching_matrix(rows, cols, vals, 'SwitchingMatrix_Unweighted.csv')
-W = form_switching_matrix(rows, cols, vals_weighted, 'SwitchingMatrix_Weighted.csv')
+W = form_switching_matrix(method_from, method_to, vals_weighted, 'SwitchingMatrix_Weighted.csv')
+
+W['Condoms'] = W['Male condom'] + W['Female condom']
+W.loc['Condoms'] = W.loc['Male condom'] + W.loc['Female condom']
+
+W['Other modern'] = W['Other modern method'] + W['Standard days method'] + W['Emergency contraception']
+W.loc['Other modern'] = W.loc['Other modern method'] + W.loc['Standard days method'] + W.loc['Emergency contraception']
+
+W['Traditional'] = W['Other traditional method'] + W['Rhythm method'] + W['Withdrawl']
+W.loc['Traditional'] = W.loc['Other traditional method'] + W.loc['Rhythm method'] + W.loc['Withdrawl']
+
+W['Termination/Stillbirth'] = W['Miscarriage'] + W['Stillbirth'] + W['Abortion']
+
+'''
+diagW = pd.Series(np.diag(W), index=[W.index, W.columns]).to_frame().unstack().fillna(0)
+print(diagW[0])
+W.sort_index(inplace=True, axis=0)
+W.sort_index(inplace=True, axis=1)
+W = W - diagW[0]
+'''
+
+W = W.reindex(['Implant', 'IUD', 'Injectables', 'Pill', 'Condoms', 'Lactational amenorrhea method (LAM)', 'Other modern', 'Traditional'], axis=0)
+W = W.reindex(['No method', 'Implant', 'IUD', 'Injectables', 'Pill', 'Condoms', 'Other modern', 'Traditional', 'Pregnancy', 'Termination/Stillbirth'], axis=1)
+
+diag_names = [name for name in W.columns.values if name in W.index.values]
+for name in diag_names:
+    W.loc[name,name] = 0
+
+w = 100*W.div(W.sum(axis=1), axis=0)
+w['Total'] = W.sum(axis=1)
+print(w)
+w.to_csv('test.csv')
 
 
-with pd.option_context('display.max_colwidth', -1):
-    print(M)
+
+exit()
+with pd.option_context('display.max_columns', -1):
+    print(W)
     exit()
 
     # Let's decode some record
-    data.iloc[range(500,600)].apply(decrypt, axis=1)
+    cal.iloc[range(500,600)].apply(decrypt, axis=1)
 
 sc.toc(start=T)
 
