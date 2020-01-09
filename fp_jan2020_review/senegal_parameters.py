3e2ca9e113da4abc6d1fcc3a3d1c6cf34bd0a251
cliffckerr <questionably@gmail.com>
2020-01-10 02:25:50 +0700

    this run works well
    Former-commit-id: 5bfd0c52d452b98d3660c41f86d071bb8caaefac

--- fp_jan2020_review/senegal_parameters.py
+++ fp_jan2020_review/senegal_parameters.py
@@ -155,8 +155,8 @@ def make_pars():
 
     # Simulation parameters
     pars['name'] = 'Default' # Name of the simulation
-    pars['n'] = 1274 # Number of people in the simulation -- from Impact 2 / 1000
-    pars['start_year'] = 1982
+    pars['n'] = 1274/2 # Number of people in the simulation -- from Impact 2 / 1000
+    pars['start_year'] = 1962
     pars['end_year'] = 2015
     pars['timestep'] = 3 # Timestep in months
     pars['verbose'] = True
@@ -168,8 +168,8 @@ def make_pars():
     pars['method_efficacy'] = default_efficacy()
     pars['barriers'] = default_barriers()
     pars['switching'] = default_switching() #pars['initial'], 
-    pars['mortality_factor'] = 1.0
-    pars['fertility_factor'] = 25 # This is suspiciously close to 12/2, but the math seems right...
+    pars['mortality_factor'] = 1.5
+    pars['fertility_factor'] = 33 # This is suspiciously close to 12/2, but the math seems right...
     pars['method_age'] = 15
     pars['max_age'] = 99
     
