2d01521e7563e117c9f5654aafaa678f51c3f5a1
cliffckerr <questionably@gmail.com>
2020-01-14 00:20:11 -0800

    fairly good calibration for now
    Former-commit-id: 0ea6bef4e2cf4386335200f7cd216e12cbada229

--- fp_jan2020_review/run_senegal.py
+++ fp_jan2020_review/run_senegal.py
@@ -11,11 +11,16 @@ do_run              = 1
 do_plot_popsize     = 1
 do_plot_pyramids    = 1
 do_plot_skyscrapers = 1
+do_plot_methods     = 1
 do_save             = 1
 
+min_age = 15
+max_age = 50
+bin_size = 5
 year_str = '2010-11'
-pop_pyr_year_fn = sp.abspath('dropbox/Population_Pyramid_-_All.csv')
-skyscrapers_fn = sp.abspath('dropbox/Skyscrapers-All-DHS.csv')
+pop_pyr_year_file = sp.abspath('dropbox/Population_Pyramid_-_All.csv')
+skyscrapers_file = sp.abspath('dropbox/Skyscrapers-All-DHS.csv')
+methods_file = sp.abspath('dropbox/Method_v312.csv')
 
 if do_run:
     pars = sp.make_pars()
@@ -24,7 +29,7 @@ if do_run:
     def add_long_acting(sim):
         print('Added long-acting intervention')
         for person in sim.people.values():
-            person.method = 'implant'
+            person.method = 'Implant'
     
     def urhi(sim):
         print('Added URHI')
@@ -68,10 +73,7 @@ if do_run:
         fig2 = pl.figure(figsize=(16,16))
         
         # Load population pyramid from DHS
-        min_age = 15
-        max_age = 50
-        bin_size = 5
-        pop_pyr_year  = pd.read_csv(pop_pyr_year_fn, header=None)
+        pop_pyr_year  = pd.read_csv(pop_pyr_year_file, header=None)
         pop_pyr_year = pop_pyr_year[pop_pyr_year[0]==year_str]
         bins = pl.arange(min_age, max_age, bin_size)
         pop_props_year = pop_pyr_year[2].to_numpy()
@@ -119,7 +121,7 @@ if do_run:
         
         # Load data
         data_parity_bins = pl.arange(0,18)
-        sky_raw_data  = pd.read_csv(skyscrapers_fn, header=None)
+        sky_raw_data  = pd.read_csv(skyscrapers_file, header=None)
         sky_raw_data = sky_raw_data[sky_raw_data[0]==year_str]
         sky_parity = sky_raw_data[2].to_numpy()
         sky_props = sky_raw_data[3].to_numpy()
@@ -138,7 +140,7 @@ if do_run:
         # Extract from model
         sky_arr['Model'] = pl.zeros((len(age_bins), len(parity_bins)))
         for person in people:
-            if not person.sex and person.age>=min_age and person.age<max_age:
+            if person.alive and not person.sex and person.age>=min_age and person.age<max_age:
                 age_bin = sc.findinds(age_bins<=person.age)[-1]
                 parity_bin = sc.findinds(parity_bins<=person.parity)[-1]
                 sky_arr['Model'][age_bin, parity_bin] += 1
@@ -186,6 +188,98 @@ if do_run:
                 pl.legend()
                 if do_save:
                     pl.savefig(sp.abspath(f'figs/senegal_age_parity_sums.png'))
+    
+    
+    if do_plot_methods:
+        data_method_counts = sc.odict().make(keys=sim.methods, vals=0.0)
+        model_method_counts = sc.dcp(data_method_counts)
+        
+        # Load data from DHS -- from dropbox/Method_v312.csv
+        data = [
+            ['Other', 'emergency contraception', 0.015216411570543636,2017.698615635373],
+            ['Condoms', 'female condom', 0.005239036180154552,2017.698615635373],
+            ['Other', 'female sterilization', 0.24609377594176307,2017.698615635373],
+            ['Implants', 'implants/norplant', 5.881839602070953,2017.698615635373],
+            ['Injectables', 'injections', 7.101718239287355,2017.698615635373],
+            ['IUDs', 'iud', 1.4865067612487317,2017.698615635373],
+            ['Lactation', 'lactational amenorrhea (lam)', 0.04745447091361792,2017.698615635373],
+            ['Condoms', 'male condom', 1.0697377418682412,2017.698615635373],
+            ['None', 'not using', 80.10054235699272,2017.698615635373],
+            ['Other', 'other modern method', 0.007832257135437748,2017.698615635373],
+            ['Traditional', 'other traditional', 0.5127850142889963,2017.698615635373],
+            ['Traditional', 'periodic abstinence', 0.393946698444533,2017.698615635373],
+            ['Pill', 'pill', 2.945874450486654,2017.698615635373],
+            ['Other', 'standard days method (sdm)', 0.06132534128612159,2017.698615635373],
+            ['Traditional', 'withdrawal', 0.12388784228417069,2017.698615635373],
+        ]
+        for entry in data:
+            data_method_counts[entry[0]] += entry[2]
+        data_method_counts[:] /= data_method_counts[:].sum()
+        
+        # From model
+        for person in people:
+            if person.alive and not person.sex and person.age>=min_age and person.age<max_age:
+                model_method_counts[person.method] += 1
+        model_method_counts[:] /= model_method_counts[:].sum()
+        
+        # Make labels
+        data_labels = data_method_counts.keys()
+        for d in range(len(data_labels)):
+            if data_method_counts[d]>0.01:
+                data_labels[d] = f'{data_labels[d]}: {data_method_counts[d]*100:0.1f}%'
+            else:
+                data_labels[d] = ''
+        model_labels = model_method_counts.keys()
+        for d in range(len(model_labels)):
+            if model_method_counts[d]>0.01:
+                model_labels[d] = f'{model_labels[d]}: {model_method_counts[d]*100:0.1f}%'
+            else:
+                model_labels[d] = ''
+        
+        # Plot pies
+        fig = pl.figure(figsize=(20,14))
+        pl.subplot(2,2,1)
+        pl.pie(data_method_counts[:], labels=data_labels)
+        pl.title('Data')
+        
+        pl.legend(data_method_counts.keys(), loc='upper right', bbox_to_anchor=(1.5,1))
+        
+        pl.subplot(2,2,2)
+        pl.pie(model_method_counts[:], labels=model_labels)
+        pl.title('Model')
+        
+        # Remake without None
+        data_method_counts['None'] = 0.0
+        model_method_counts['None'] = 0.0
+        data_method_counts[:] /= data_method_counts[:].sum()
+        model_method_counts[:] /= model_method_counts[:].sum()
+        
+        # Make labels
+        data_labels = data_method_counts.keys()
+        for d in range(len(data_labels)):
+            if data_method_counts[d]>0.01:
+                data_labels[d] = f'{data_labels[d]}: {data_method_counts[d]*100:0.1f}%'
+            else:
+                data_labels[d] = ''
+        model_labels = model_method_counts.keys()
+        for d in range(len(model_labels)):
+            if model_method_counts[d]>0.01:
+                model_labels[d] = f'{model_labels[d]}: {model_method_counts[d]*100:0.1f}%'
+            else:
+                model_labels[d] = ''
+        
+        pl.subplot(2,2,3)
+        pl.pie(data_method_counts[:], labels=data_labels)
+        pl.title('Data -- excluding no method')
+        
+        # pl.legend(data_method_counts.keys(), loc='upper right', bbox_to_anchor=(1,1.5))
+        
+        pl.subplot(2,2,4)
+        pl.pie(model_method_counts[:], labels=model_labels)
+        pl.title('Model -- excluding no method')
+        
+        if do_save:
+            pl.savefig(sp.abspath(f'figs/senegal_method_mix.png'))
     
     
             
