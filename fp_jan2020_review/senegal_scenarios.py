9d9373167ea824db7c879e7ea786460e5e43d2f3
cliffckerr <questionably@gmail.com>
2020-01-15 14:35:04 -0800

    starting on scenarios
    Former-commit-id: 5d7ea52f1448d73720ecb431d17d3ccd3094984c

---
+++ fp_jan2020_review/senegal_scenarios.py
@@ -0,0 +1,17 @@
+'''
+Run Senegal scenarios
+'''
+
+import pylab as pl
+import pandas as pd
+import sciris as sc
+import lemod_fp as lfp
+import senegal_parameters as sp
+
+# Set what to run
+run_implants = 1
+run_health   = 0
+
+
+if run_implants:
+    
