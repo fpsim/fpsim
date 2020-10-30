0be24a6075187205546a125fa02af045131d809f
cliffckerr <questionably@gmail.com>
2020-10-30 13:29:22 -0700

    enable tests
    Former-commit-id: 2b32186c764b6d9afdbc6edd32e972512127cb43

--- tests/test_analyses.py
+++ tests/test_analyses.py
@@ -11,7 +11,8 @@ import importlib.util as iu
 pl.switch_backend('agg') # To avoid graphs from appearing -- if you want them, run the examples directly
 cwd = Path(sc.thisdir(__file__))
 analyses_dir = cwd.joinpath('../fp_analyses')
-sys.path.append(analyses_dir) # To enable imports to work
+sys.path.append(str(analyses_dir)) # To enable imports to work
+
 
 def run_script(name):
     '''
@@ -23,15 +24,17 @@ def run_script(name):
     spec = iu.spec_from_file_location("__main__", analyses_dir.joinpath(f"{name}.py"))
     module = iu.module_from_spec(spec)
     spec.loader.exec_module(module)
+    return
 
 
 def test_run_senegal():
-    # import run_senegal
     run_script("run_senegal")
+    return
 
-# def test_run_calibration():
-#     import run_calibration
-    # run_script("run_calibration")
+
+def test_run_calibration():
+    run_script("run_calibration")
+    return
 
 
 #%% Run as a script
@@ -40,7 +43,7 @@ if __name__ == '__main__':
     T = sc.tic()
 
     test_run_senegal()
-    # test_run_calibration()
+    test_run_calibration()
 
     sc.toc(T)
     print('Done.')
