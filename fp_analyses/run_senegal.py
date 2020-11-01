43b99bb735457d88a69f7eb2185ce073d54d56d4
Annie Valente <annievalente@gmail.com>
2020-11-01 16:46:25 -0500

    Add mcpr file, pop_size fix
    Add mcpr file for senegal and try to fix population growth plotting of data for now
    
    
    Former-commit-id: 1ecff60ec935fc331cf722861b06ee40e51ee513

--- fp_analyses/run_senegal.py
+++ fp_analyses/run_senegal.py
@@ -24,7 +24,7 @@ do_plot_model_pyramid = 1
 do_plot_skyscrapers = 1
 do_plot_methods     = 1
 do_plot_spacing     = 1
-do_save             = 0
+do_save             = 1
 
 min_age = 15
 max_age = 50
@@ -44,6 +44,7 @@ methods_file = datapath('Method_v312.csv')
 spacing_file = datapath('BirthSpacing.csv')
 popsize_file = datapath('senegal-popsize.csv')
 barriers_file = datapath('DHSIndividualBarriers.csv')
+mcpr_file = datapath('mcpr_senegal.csv')
 
 
 if do_run:
@@ -99,7 +100,7 @@ if do_run:
         dhs = dhs.rename(columns={'v012': 'Age', 'v213': 'Pregnant',
                                   'v218': 'Parity'})  # Parity means # of living children in DHS
 
-        fig, axes = pl.subplots(3, 2, figsize = (18, 14))
+        fig, axes = pl.subplots(3, 2, figsize = (16, 12))
 
         #fig.suptitle('FP Sim Model vs DHS data on age, pregnancy, and parity')
 
@@ -122,37 +123,40 @@ if do_run:
     if do_plot_popsize:
 
         # Load data
-        popsize_tfr  = pd.read_csv(popsize_file, header=None)
+        popsize_tfr = pd.read_csv(popsize_file, header=None)
+        mcpr = pd.read_csv(mcpr_file, header = None)
 
         # Handle population size
         pop_years = popsize_tfr.iloc[0,:].to_numpy()
-
         popsize = popsize_tfr.iloc[1,:].to_numpy() / sim.pars['n'] # Conversion factor from Senegal to 500 people, = 1 / 1000 * 1.4268 / 500
+        mcpr_years = mcpr.iloc[:,0].to_numpy()
+        mcpr_rates = mcpr.iloc[:,1].to_numpy()
 
+        '''
         # Default plots
         fig = sim.plot()
 
         sim.plot_postpartum()
-
+        '''
+        fig = pl.figure(figsize=(16, 16))
+        pl.subplot(2, 1, 1)
+        pl.scatter(pop_years, popsize, c='k', label='Data', zorder=1000)
         # Population size plot
-        ax = fig.axes[0] # First axis on plot
-        ax.scatter(pop_years, popsize, c='k', label='Data', zorder=1000)
-        #pl.legend()
+        #ax = fig.axes[1,0] # First axis on plot
+
+        pl.legend()
+
+        pl.subplot(2, 1, 2) # Second axis on plot
+        pl.scatter(mcpr_years, mcpr_rates, c='k', label='Data', zorder=1000)
+        pl.legend()
+
         if do_save:
             pl.savefig(sp.abspath('figs/senegal_popsize.png'))
-
-        # MCPR -- TODO, copied from senegal_parameters.py
-        mcpr_years = pl.array([1986, 1992, 1997, 2005, 2010, 2012, 2014, 2015, 2016, 2017])
-        mcpr_rates = pl.array([2.65, 4.53, 7.01, 7.62, 8.85, 11.3, 14.7, 15.3, 16.5, 18.8])
-
-        ax = fig.axes[1] # Second axis on plot
-        ax.scatter(mcpr_years, mcpr_rates, c='k', label='Data', zorder=1000)
-        #pl.legend()
 
         #350434.0 <--- Factor previously used to adjust population
 
     if do_plot_pyramids:
-        fig2 = pl.figure(figsize=(16,16))
+        fig = pl.figure(figsize=(16,16))
 
         # Load population pyramid from DHS
         pop_pyr_year  = pd.read_csv(pop_pyr_year_file, header=None)
@@ -395,7 +399,7 @@ if do_run:
         # From data
         data = pd.read_csv(spacing_file)
 
-        right_year = data['SurveyYear']=='2010-11'
+        right_year = data['SurveyYear']=='2017'
         not_first = data['Birth Order'] != 0
         is_first = data['Birth Order'] == 0
         filtered = data[(right_year) & (not_first)]
