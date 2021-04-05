d5504ad105ef441ae85805aac46f9c358cc7fbb1
cliffckerr <questionably@gmail.com>
2021-04-05 14:00:48 -0700

    example working
    Former-commit-id: c2772796bb9eb7183c93ab4cfc70eaace208c8e0

--- fp_analyses/example_parallel.py
+++ fp_analyses/example_parallel.py
@@ -6,7 +6,7 @@ import sciris as sc
 import fpsim as fp
 import senegal_parameters as sp
 
-def run(seed=1, n=100):
+def run(seed=1, n=200):
     pars = sp.make_pars()
     pars['seed'] = seed
     pars['n'] = n
@@ -14,4 +14,19 @@ def run(seed=1, n=100):
     sim.run()
     return sim
 
-sims = sc.parallelize(run, range(10))
+
+if __name__ == '__main__':
+
+    T = sc.tic()
+    sims = sc.parallelize(run, range(5))
+    all_ppl = sc.mergedicts(*[s.people for s in sims])
+    msim = sc.dcp(sims[0])
+    msim.people = all_ppl
+    for k in msim.results.keys():
+        for s,sim in enumerate(sims):
+            if s>0:
+                msim.results[k] += sim.results[k]
+
+    msim.plot()
+    sc.toc(T)
+    print('Done.')
