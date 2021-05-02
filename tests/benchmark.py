32152c27377732df3774d2717fe970b696986e39
cliffckerr <questionably@gmail.com>
2021-05-02 03:24:32 -0700

    most of the time is in reshaping arrays
    Former-commit-id: 156ff98c929675808698a7703beb7d4319702265

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -8,18 +8,17 @@ from fp_analyses import senegal_parameters as sp
 
 pars = sp.make_pars()
 sim = fp.Sim(pars)
-person = sc.odict(sim.people)[0]
-to_profile = 'sim'
+to_profile = 'update'
 
 func_options = {'sim':        sim.run,
-                'update':     person.update,
+                'update':     sim.people.update,
                 # 'preg':       person.get_preg_prob,
                 # 'get_method': person.get_method,
                 }
 
 def run():
     pars = sp.make_pars()
-    pars['n'] = 100
+    pars['n'] = 500
     sim = fp.Sim(pars)
     sim.run()
     return
