a0d9e3c7bc9a47516bf3c124f4996480ceba49ed
cliffckerr <questionably@gmail.com>
2021-06-10 22:37:49 -0700

    add comments
    Former-commit-id: a8083d713aebeb7808aa7bc4268ff9d2e7b1d0ed

--- tests/test_multisim.py
+++ tests/test_multisim.py
@@ -13,17 +13,18 @@ def test_multisim(do_plot=False):
     ''' Try running a multisim '''
     sc.heading('Testing multisim...')
 
+    # Generate sims in a loop
     sims = []
     for i in range(3):
         pars = fa.senegal_parameters.make_pars()
         pars['n'] = 500
         pars['verbose'] = 0.1
-        pars['exposure_correction'] = 0.5 + 0.5*i
+        pars['exposure_correction'] = 0.5 + 0.5*i # Run a sweep over exposure
         sim = fp.Sim(pars=pars)
         sims.append(sim)
 
     msim = fp.MultiSim(sims)
-    msim.run()
+    msim.run() # Run sims in parallel
 
     births = msim.results.births
     assert sum(births.low) < sum(births.high), 'Expecting the higher bound of births to be higher than the lower bound'
