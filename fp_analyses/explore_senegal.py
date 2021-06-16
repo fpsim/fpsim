c493d08981c7640dc2024b0d0adafbec0ef51f45
MObrien-IDM <79864181+MObrien-IDM@users.noreply.github.com>
2021-06-16 10:47:08 -0700

    Changes to output (aligned with prev. toggle branch)
    Former-commit-id: b6e347b864f8f6b29d88d90d41265dbf3ad10529

--- fp_analyses/explore_senegal.py
+++ fp_analyses/explore_senegal.py
@@ -86,15 +86,41 @@ if demographics:
                     pl.sum(results['infant_deaths'][-12:]) +\
                     pl.sum(results['maternal_deaths'][-12:])
     print(f'Crude death rate per 1,000 inhabitants: {(total_deaths/results["pop_size"][-1])*1000}')
-    infant_deaths = pl.sum(results['infant_deaths'][-12:])
-    maternal_deaths = pl.sum(results['maternal_deaths'][-36:])
-    births_last_year = pl.sum(results['births'][-12:])
-    births_last_3_years = pl.sum(results['births'][-36:])
-    print(f'Total infant mortality rate in model: {(infant_deaths/births_last_year)*1000}.  Infant mortality rate 2015 Senegal: 36.4')
-    print(f'Total maternal death rate in model: {(maternal_deaths/births_last_3_years)*100000}.  Maternal mortality ratio 2015 Senegal: 315 ')
-    print(f'Crude birth rate per 1000 inhabitants in model: {(births_last_year/results["pop_size"])*1000}.  Crude birth rate Senegal 2018: 34.52 per 1000 inhabitants')
-    print(f'Final percent non-postpartum : {results["nonpostpartum"][-1]}')
-    print(f'TFR rates over last 10 years: {results["tfr_rates"][-10:]}.  TFR in Senegal in 2015: 4.84')
+
+    x = results['t']
+    y1 = results['deaths']
+    y2 = results['infant_deaths']
+    y3 = results['maternal_deaths']
+    y4 = results['births']
+#Line graph
+    pl.plot(x, y1, label = "deaths")
+    pl.plot(x, y2, label = "infant deaths")
+    pl.plot(x, y3, label = "maternal deaths")
+
+    pl.xlabel("Year")
+    pl.ylabel("Deaths")
+    pl.legend(loc="best")
+    pl.title('Monthly mortality by type, 2010-2019', fontweight = 'bold')
+    pl.xlim([2010,2019])
+    pl.show()
+
+
+
+
+
+
+infant_deaths = pl.sum(results['infant_deaths'][-12:])
+maternal_deaths1 = pl.sum(results['maternal_deaths'][-12:])
+maternal_deaths3 = pl.sum(results['maternal_deaths'][-36:])
+maternal_deaths7 = pl.sum(results['maternal_deaths'][-84:])
+births_last_year = pl.sum(results['births'][-12:])
+births_last_3_years = pl.sum(results['births'][-36:])
+births_last_7_years = pl.sum(results['births'][-84:])
+all_maternal_deaths = pl.sum(results['maternal_deaths'])
+all_births = pl.sum(results['births'])
+births_2017 = pl.sum(results['births'][-48:-36])
+maternal_deaths_2017 = pl.sum(results['births'][-48:-36])
+
 
 if birth_spacing:
 
