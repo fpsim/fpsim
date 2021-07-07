46ded50a95f53248f678d49eaa9891d6276b4935
cliffckerr <questionably@gmail.com>
2021-07-07 11:05:05 -0400

    update maternal mortality calculation
    Former-commit-id: 665590c788eb72244515068dc8f7f6b733281927

--- fp_analyses/senegal_parameters.py
+++ fp_analyses/senegal_parameters.py
@@ -776,7 +776,7 @@ def make_pars(configuration_file=None, defaults_file=None):
     pars['method_efficacy']    = default_efficacy()
     pars['method_efficacy25'] = default_efficacy25()
     pars['barriers']           = default_barriers()
-    pars['maternal_mortality'] = default_maternal_mortality(get_parameter(parameters=input_parameters, parameter="maternal_mortality_multiplier", defaults=default_parameters))
+    pars['maternal_mortality'] = default_maternal_mortality()
     pars['infant_mortality']   = default_infant_mortality()
     pars['sexual_activity']    = default_sexual_activity() # Returns linear interpolation of annual sexual activity based on age
     pars['sexual_activity_postpartum'] = default_sexual_activity_postpartum() # Returns array of likelihood of resuming sex per postpartum month
