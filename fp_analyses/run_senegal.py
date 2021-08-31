651501dcfccbf05831860929211b93448b3df0cb
Annie Valente <annievalente@gmail.com>
2021-08-31 13:54:58 -0400

    change parity definition in 'run_senegal' file
    Former-commit-id: fe8cbed368cf95819554cd0c550041fa661c6003

--- fp_analyses/run_senegal.py
+++ fp_analyses/run_senegal.py
@@ -110,7 +110,7 @@ if do_run:
         dhs = pd.read_stata(pregnancy_parity_file, convert_categoricals=False)
         dhs = dhs[['v012', 'v213', 'v218']]
         dhs = dhs.rename(columns={'v012': 'Age', 'v213': 'Pregnant',
-                                  'v218': 'Parity'})  # Parity means # of living children in DHS
+                                  'v201': 'Parity'})  # Parity means # of living children in DHS
 
         fig, axes = pl.subplots(3, 2, figsize = (16, 12))
 
