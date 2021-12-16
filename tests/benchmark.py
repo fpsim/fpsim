c34005fc81f455ead3c732ac7206f45bab988c8a
cliffckerr <questionably@gmail.com>
2021-12-15 19:55:35 -0800

    no obvious benchmarking improvements
    Former-commit-id: 46ee17e848ca7499e77ac9298b88f65b570bd7e7

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -6,21 +6,23 @@ import sciris as sc
 import fpsim as fp
 from fp_analyses import senegal_parameters as sp
 
-do_profile = 0
+do_profile = 1
 pars = sp.make_pars()
 sim = fp.Sim(pars)
-to_profile = 'run'
 
-func_options = {
-    'run':         sim.run,
-    'update':      sim.people.update,
-    'people_init': sim.people.__init__,
-    'other':       [fp.model.arr][0],
-}
+ppl = sim.people
+to_profile = sc.objdict(
+    run =         sim.run,
+    update =      ppl.update, # 86%
+    people_init = ppl.__init__,
+    contra =      ppl.update_contraception, # 46%
+    method_pp =   ppl.get_method_postpartum, # 56%, no obvious performance improvements
+    get_method =  ppl.get_method, # 46%, could maybe be merged with previous
+)['get_method']
 
 def run():
     pars = sp.make_pars()
-    pars['n'] = 50000
+    pars['n'] = 10000
     sim = fp.Sim(pars)
     sim.run()
     return sim
@@ -28,7 +30,7 @@ def run():
 
 if __name__ == '__main__':
     if do_profile:
-        sc.profile(run, func_options[to_profile])
+        sc.profile(run, to_profile)
     else:
         sc.tic()
         sim = run()
