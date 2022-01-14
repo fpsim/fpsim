e2e7e83fa681e6ec96ffb78c3d3a6df6f5ecd2da
cliffckerr <questionably@gmail.com>
2022-01-14 12:24:30 -0500

    temporary revert
    Former-commit-id: c5e9da3cc9ff74b40e8cf120d77710edf59b7a69

--- fp_2022jan_gr/animation_schematic.py
+++ fp_2022jan_gr/animation_schematic.py
@@ -58,37 +58,13 @@ yy = yy.flatten()
 
 sc.options(dpi=200)
 fig,ax = pl.subplots(figsize=(6,6))
-pl.axis('off')
-δ = 0.5
-pl.xlim((-δ, n_side-1+δ))
-pl.ylim((-δ, n_side-1+δ))
 sc.figlayout()
 
-frames = sc.autolist()
-
-        for i in range(nframes): # Loop over the frames
-            dots += pl.randn(ndots, 2) # Move the dots randomly
-            color = pl.norm(dots, axis=1) # Set the dot color
-            old = pl.array(old_dots) # Turn into an array
-            plot1 = pl.scatter(old[:,0], old[:,1], c='k') # Plot old dots in black
-            plot2 = pl.scatter(dots[:,0], dots[:,1], c=color) # Note: Frames will be separate in the animation
-            pl.xlim((-axislim, axislim)) # Set x-axis limits
-            pl.ylim((-axislim, axislim)) # Set y-axis limits
-            kwargs = {'transform':pl.gca().transAxes, 'horizontalalignment':'center'} # Set the "title" properties
-            title = pl.text(0.5, 1.05, f'Iteration {i+1}/{nframes}', **kwargs) # Unfortunately pl.title() can't be dynamically updated
-            pl.xlabel('Latitude') # But static labels are fine
-            pl.ylabel('Longitude') # Ditto
-            frames.append((plot1, plot2, title)) # Store updated artists
-            old_dots = pl.vstack([old_dots, dots]) # Store the new dots as old dots
-        sc.savemovie(frames, 'fleeing_dots.mp4', fps=20, quality='high') # Save movie as a high-quality mp4
-
-
 for entry in data.values():
-    frame = sc.autolist()
     pl.cla()
     # cc = sc.vectocolor(entry.method[:n], maxval=max_method)
     cc = entry.method[:n] > 0
-    frame += pl.scatter(xx, yy, s=100, c=cc, marker='o')
+    pl.scatter(xx, yy, s=100, c=cc, marker='o')
     for m, (ix,iy) in enumerate(zip(xx,yy)):
         n_children = len(entry.children[m])
         if n_children:
@@ -96,13 +72,12 @@ for entry in data.values():
             rad = 2*np.pi*c_arr/max_children
             dx = radius*np.cos(rad)
             dy = radius*np.sin(rad)
-            frame += pl.scatter(ix+dx, iy+dy, s=10, c='k')
+            pl.scatter(ix+dx, iy+dy, s=10, c='k')
 
-
-
-            kwargs = {'transform':pl.gca().transAxes, 'horizontalalignment':'center'} # Set the "title" properties
-            title = pl.text(0.5, 1.05, f'Iteration {i+1}/{nframes}', **kwargs) # Unfortunately pl.title() can't be dynamically updated
-
+    δ = 0.5
+    pl.axis('off')
+    pl.xlim((-δ, n_side-1+δ))
+    pl.ylim((-δ, n_side-1+δ))
     pl.title(f'{entry.y:0.1f}')
     pl.pause(0.01)
 
