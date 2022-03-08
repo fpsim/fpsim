2738101fff719c408704481bc3536c48c9268e41
Sam Buxton <67336501+SBuxton-IDM@users.noreply.github.com>
2022-03-07 16:49:14 -0800

    Added test coverage for update_methods
    Former-commit-id: 312b01bd7af165bb47f2ba7be95621fb71a88d9e

--- tests/test_interventions.py
+++ tests/test_interventions.py
@@ -52,14 +52,71 @@ def test_analyzers():
 
     return sim
 
+def test_update_matrix():
+    """
+    Checks that fp.update_methods() function properly updates sim.pars for
+    both the selected age keys, and the type (methods or postpartum_methods) of
+    transition matrix
+    """
+    low_eff = dict(dist='uniform', par1=0.80, par2=0.90)
+    pars_no_keys_methods = fa.senegal_parameters.make_pars()
+    pars_keys_methods = fa.senegal_parameters.make_pars()
+    pars_no_keys_pp = fa.senegal_parameters.make_pars()
+    pars_keys_pp = fa.senegal_parameters.make_pars()
+    scen_no_keys = sc.objdict(
+        eff = {'Other modern':low_eff}, # Co-opt an unused method and simulate a medium-efficacy method
+        probs = [
+            dict(
+                source = 'None', # Source method, 'all' for all methods
+                dest   = 'Other modern', # Destination
+                factor = None, # Factor by which to multiply existing probability
+                value  = 0.2 # Alternatively, specify the absolute probability of switching to this method
+            ),
+        ]
+    )
+    scen_keys = sc.objdict(
+        eff = {'Other modern':low_eff}, # Co-opt an unused method and simulate a medium-efficacy method
+        probs = [
+            dict(
+                source = 'None', # Source method, 'all' for all methods
+                dest   = 'Other modern', # Destination
+                factor = None, # Factor by which to multiply existing probability
+                value  = 0.2, # Alternatively, specify the absolute probability of switching to this method
+                keys   = ['<18','18-20'], # Which age keys to modify -- if not specified, all
+            ),
+        ]
+    )
+
+    uptake_no_keys_methods = fp.update_methods(2005, scen_no_keys, matrix='probs_matrix') # Create intervention
+    uptake_keys_methods = fp.update_methods(2005, scen_keys, matrix='probs_matrix') # Create intervention
+    uptake_no_keys_pp = fp.update_methods(2005, scen_no_keys, matrix='probs_matrix_1-6') # Create intervention
+    uptake_keys_pp = fp.update_methods(2005, scen_keys, matrix='probs_matrix_1-6') # Create intervention
+
+    interventions = [uptake_no_keys_methods, uptake_keys_methods, uptake_no_keys_pp, uptake_keys_pp]
+
+    simlist = []
+    for index, pars in enumerate([pars_no_keys_methods, pars_keys_methods, pars_no_keys_pp, pars_keys_pp]):
+        pars.update(interventions=interventions[index])
+        simlist.append(fp.Sim(pars=pars))
+    
+    msim = fp.MultiSim(sims=simlist)
+    msim.run()
+
+    assert msim.sims[0].pars['methods']['probs_matrix']['21-25'][0][9] != msim.sims[1].pars['methods']['probs_matrix']['21-25'][0][9]
+    assert msim.sims[0].pars['methods']['probs_matrix']['21-25'][0][9] == 0.2
+    #assert (msim.sims[0].pars['methods_postpartum'] == msim.sims[1].pars['methods_postpartum']).all()
+    #assert (msim.sims[2].pars['methods'] == msim.sims[3].pars['methods']).all()
+    assert msim.sims[2].pars['methods_postpartum']['probs_matrix_1-6']['21-25'][0][9] != msim.sims[3].pars['methods_postpartum']['probs_matrix_1-6']['21-25'][0][9]
+    assert msim.sims[2].pars['methods_postpartum']['probs_matrix_1-6']['21-25'][0][9] == 0.2
 
 if __name__ == '__main__':
 
     # Start timing and optionally enable interactive plotting
     T = sc.tic()
 
-    sim1 = test_interventions()
-    sim2 = test_analyzers()
+    #sim1 = test_interventions()
+    #sim2 = test_analyzers()
+    test_update_matrix()
 
     print('\n'*2)
     sc.toc(T)
