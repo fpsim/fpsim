8f340f08a40ba13f3a628a20417dbdf2dce298ed
Annie Valente <annievalente@gmail.com>
2022-03-30 11:06:45 -0400

    erroring, need to figure out findnearest to return an array
    Former-commit-id: a52688de3e55e7a50140faaac1e1015f4223e1a2

--- fpsim/sim.py
+++ fpsim/sim.py
@@ -390,8 +390,9 @@ class People(fpb.BasePeople):
 
     def infant_mortality(self):
         '''Check for probability of infant mortality (death < 1 year of age)'''
-        age_gradient = self.pars['infant_mortality']['age_gradient'][self.int_age_clip]
-        death_prob = (self.pars['mortality_probs']['infant']) * age_gradient
+        age_inds = sc.findnearest(self.pars['infant_mortality']['ages'], self.age)
+        death_prob = (self.pars['mortality_probs']['infant'])
+        death_prob = death_prob * (self.pars['infant_mortality']['age_probs'][age_inds])
         is_death = self.binomial(death_prob)
         death = self.filter(is_death)
         self.step_results['infant_deaths'] += len(death)
