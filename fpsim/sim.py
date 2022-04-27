6b2ef28f2a5df73908ae7c95598b11e3528cc5c0
Sam Buxton <67336501+SBuxton-IDM@users.noreply.github.com>
2022-04-27 16:51:43 -0700

    Quick fix
    Former-commit-id: 084574a7f244f9f865de2261734a7f2975ae83b3

--- fpsim/sim.py
+++ fpsim/sim.py
@@ -487,7 +487,6 @@ class People(fpb.BasePeople):
         for i in live.inds: # Handle DOBs
             all_ppl.dobs[i].append(all_ppl.age[i])  # Used for birth spacing only, only add one baby to dob -- CK: can't easily turn this into a Numpy operation
             if len(all_ppl.dobs[i]) == 1: # if they didn't give birth but already had one baby, age at first birth would update to current age
-                print("Baby is born!")
                 all_ppl.first_birth_age[i] = all_ppl.age[i]
         for i in stillborn.inds: # Handle adding dates
             all_ppl.still_dates[i].append(all_ppl.age[i])
