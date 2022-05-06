67039497eed8803b32ff08dd3bcc8e413a1d9c41
cliffckerr <questionably@gmail.com>
2022-05-06 01:38:58 -0400

    more refactoring
    Former-commit-id: ae55f03705f153d8e4e3de7ebfc1c3bfe339261e

--- tests/test_contraceptives.py
+++ tests/test_contraceptives.py
@@ -1,18 +1,17 @@
-import fpsim as fp
-import unittest
-import json
-from copy import deepcopy
+import os
+import sys
 import pylab as pl
 import numpy as np
-import pytest
-import sys
-import os
+import sciris as sc
+import fpsim as fp
+import unittest
+
 
 @unittest.skip("Need to optimize with multisim before it can be in GHA")
 class TestContraceptiveEfficacy(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.pars pars = fp.pars(n=500)
+        self.pars = fp.pars(n=500)
         self.contraceptives =  [
             "None",
             "Pill",
@@ -36,7 +35,7 @@ class TestContraceptiveEfficacy(unittest.TestCase):
             method::str
                 method name, for example 'BTL'
         """
-        base_pars = deepcopy(self.pars)
+        base_pars = sc.dcp(self.pars)
         method_index = self.pars['methods']['map'][method]
 
         prob_dict = {}
@@ -59,7 +58,7 @@ class TestContraceptiveEfficacy(unittest.TestCase):
             self.pars["method_efficacy"] = np.array(efficacy)
             sim = fp.SimVerbose(self.pars)
             sims.append(sim)
-        
+
         multi = fp.MultiSim(sims=sims)
         multi.run()
         for sim in multi.sims:
@@ -83,16 +82,16 @@ class TestContraceptiveEfficacy(unittest.TestCase):
     def test_efficacy_condoms(self):
         self.all_but("Condoms")
 
-    def test_efficacy_condoms(self):
+    def test_efficacy_rythm(self):
         self.all_but("Rhythm")
 
-    def test_efficacy_condoms(self):
+    def test_efficacy_withdrawal(self):
         self.all_but("Withdrawal")
 
-    def test_efficacy_condoms(self):
+    def test_efficacy_implants(self):
         self.all_but("Implants")
 
-    def test_efficacy_condoms(self):
+    def test_efficacy_other(self):
         self.all_but("Other")
 
     def test_efficacy_BTL(self):
