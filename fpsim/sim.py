138a4db96dbbbaaf178af6d67c0697fbaebff39a
cliffckerr <questionably@gmail.com>
2022-05-10 18:48:06 -0700

    still working
    Former-commit-id: 1c7bcb8ee147c78ef2bd3cea23927de218746aa3

--- fpsim/sim.py
+++ fpsim/sim.py
@@ -208,8 +208,7 @@ class People(fpb.BasePeople):
             for m in pp_methods['map'].values():
                 match_m    = (orig_methods == m)
                 match = match_m * match_postpartum_age
-                # this_method = self.filter(match)
-                old_method = self.method[match].copy()
+                old_method = self.method[match]
 
                 matrix = pp_switch[key]
                 choices = matrix[m]
