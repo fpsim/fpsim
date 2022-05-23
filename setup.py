416be330645715d4763878e76914c1057201b7a3
cliffckerr <questionably@gmail.com>
2022-05-23 01:16:32 -0400

    update setup
    Former-commit-id: b629ba663e4316dca700743fcfee7b62b8d758b7

--- setup.py
+++ setup.py
@@ -7,14 +7,18 @@ cwd = os.path.abspath(os.path.dirname(__file__))
 versionpath = os.path.join(cwd, 'fpsim', 'version.py')
 version = runpy.run_path(versionpath)['__version__']
 
+# Get the documentation
+with open(os.path.join(cwd, 'README.rst'), "r") as f:
+    long_description = f.read()
+
 CLASSIFIERS = [
     "Environment :: Console",
     "Intended Audience :: Science/Research",
-    "License :: OSI Approved :: GPLv3",
+    "License :: Other/Proprietary License",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Topic :: Software Development :: Libraries :: Python Modules",
-    "Development Status :: 1",
+    "Development Status :: 5 - Production/Stable",
     "Programming Language :: Python :: 3.9",
 ]
 
@@ -24,6 +28,9 @@ setup(
     author="Michelle O'Brien, Annie Valente, Cliff Kerr, Sam Buxton, Daniel Klein, Marita Zimmermann",
     author_email="info@fpsim.org",
     description="FPsim: Family Planning Simulator",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url='http://fpsim.org',
     keywords=["family planning", "women's health", "agent-based model", "simulation"],
     platforms=["OS Independent"],
     classifiers=CLASSIFIERS,
