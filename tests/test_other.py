4b7da370756ccd5b05d91439af6deaa925821459
cliffckerr <questionably@gmail.com>
2022-05-31 22:24:14 -0400

    add plot people test coverage
    Former-commit-id: fb7f5d8ade6df89c704192b9cb128ab520b6c32c

--- tests/test_other.py
+++ tests/test_other.py
@@ -7,6 +7,9 @@ import sciris as sc
 import fpsim as fp
 
 
+do_plot  = 1 # Whether to do plotting in interactive mode
+sc.options(backend='agg') # Turn off interactive plots
+
 def ok(string):
     ''' Print out a successful test nicely '''
     return sc.printgreen(f'✓ {string}\n')
@@ -40,13 +43,28 @@ def test_to_df():
     sc.heading('Testing other sim methods...')
 
     sim = fp.Sim(location='test').run()
+    sim.brief()
+    ok('sim.brief() worked')
+
     df = sim.to_df()
     births = df.births.sum()
     last = df.t.values[-1]
     assert last == sim['end_year'], 'Last years do not match'
     assert births > 0, 'Expected births'
     ok(f'to_df() worked to capture {births} births and final year {last}')
+
     return df
+
+
+def test_plot_people():
+    sc.heading('Test plotting people...')
+
+    sim = fp.Sim(location='test').run()
+
+    if do_plot:
+        sim.people.plot()
+
+    return sim.people
 
 
 # Run all tests
@@ -57,3 +75,4 @@ if __name__ == '__main__':
     with sc.timer():
         opts = test_options()
         df   = test_to_df()
+        ppl  = test_plot_people()
