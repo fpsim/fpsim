31e53d884a77dc9b3b061719b6e7762c7f9549f3
MObrien-IDM <79864181+MObrien-IDM@users.noreply.github.com>
2022-06-29 16:57:07 -0700

    add pregs to defaults
    Former-commit-id: ce9704fb26876aa6710a0d140642628f08487df4

--- fpsim/defaults.py
+++ fpsim/defaults.py
@@ -27,6 +27,7 @@ person_defaults = dict(
     stillbirth           = 0,
     miscarriage          = 0,
     abortion             = 0,
+    pregnancies          = 0,
     remainder_months     = 0,
     breastfeed_dur       = 0,
     breastfeed_dur_total = 0,
