f93e7847b6036e864fac6270f7d6fbe4244e01a7
Sam Buxton <67336501+SBuxton-IDM@users.noreply.github.com>
2022-07-18 13:13:46 -0700

    Reverting version
    Former-commit-id: d32c2cdf4e06a81a1d0d6aa003d7e9391b6077c4

--- fpsim/version.py
+++ fpsim/version.py
@@ -1,3 +1,3 @@
-__version__ = '0.17.2'
-__versiondate__ = '2022-07-15'
+__version__ = '0.17.1'
+__versiondate__ = '2022-07-01'
 __license__ = f'FPsim {__version__} ({__versiondate__}) — © 2019-2022 by IDM'
