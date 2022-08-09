87a57f9fbf02205549aa187c88714d1f379b92bc
Sam Buxton <67336501+SBuxton-IDM@users.noreply.github.com>
2022-08-09 16:01:01 -0700

    Switched to np implmentation
    Former-commit-id: a5bb02b25b0ee210c2fdf5564ee17b5c5eef4096

--- fpsim/sim.py
+++ fpsim/sim.py
@@ -518,8 +518,14 @@ class People(fpb.BasePeople):
             deliv.postpartum_dur = 0
 
             # Handle stillbirth
+            def find_nearest(array, value): # can handle multiple values
+                idx = np.searchsorted(array, value, side="left")
+                prev_idx_is_less = ((idx == len(array))|(np.fabs(value - array[np.maximum(idx-1, 0)]) < np.fabs(value - array[np.minimum(idx, len(array)-1)])))
+                idx[prev_idx_is_less] -= 1
+                return idx
+
             still_prob = self.pars['mortality_probs']['stillbirth']
-            age_ind = [np.argmin(np.abs(self.pars['stillbirth_rate']['ages'] - age)) for age in deliv.age]
+            age_ind = find_nearest(self.pars['stillbirth_rate']['ages'], deliv.age)
             still_prob = still_prob * (self.pars['stillbirth_rate']['age_probs'][age_ind]) if len(self) > 0 else 0
 
             is_stillborn = deliv.binomial(still_prob)    
