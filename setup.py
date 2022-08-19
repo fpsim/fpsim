6f8dccfd69803769c8fc925e818b7268a7fe1a1a
Jen Schripsema <jen.schripsema@gatesfoundation.org>
2022-08-18 18:05:03 -0600

    set scipy version to <1.9.0 for version incompatibility with optuna
    Former-commit-id: bb8a37c4fd1426a16f82a049a386d325eab5d48f

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=[
         'numpy',
         'numba',
-        'scipy',
+        'scipy<1.9.0',
         'pandas>=1.3', 
         'sciris>=1.2.1',
         'matplotlib>=3.5.0',
