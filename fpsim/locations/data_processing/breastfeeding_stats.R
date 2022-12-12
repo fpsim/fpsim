d2cee8cade8c895e769567097c308d7bf02b885f
MObrien-IDM <79864181+MObrien-IDM@users.noreply.github.com>
2022-12-12 15:20:56 -0800

    added option for survey weights

--- fpsim/locations/data_processing/breastfeeding_stats.R
+++ fpsim/locations/data_processing/breastfeeding_stats.R
@@ -7,14 +7,24 @@ filename <- "filename" ## must be a KR (child recode) file from the DHS for auto
 setwd(dir)
 dat <- read.dta13(filename)
 
-require(ggplot2)
+require(fitdistrplus)
 
-dat_nonmiss <- subset(dat, m5 < 93) #m5 = months of breastfeeding in previous pregnancy
+dat_nonmiss <- subset(dat, m4 < 93) #m5 = months of breastfeeding in previous pregnancy
+dat_nonmiss$m4_weighted <- (dat_nonmiss$v005/1000000) * dat_nonmiss$m4 #use this code to apply survey weights
 
-fitgumbel <- fitdist(dat_nonmiss$m5, "gumbel", start=list(a=1, b=1))
+
+#gumbel distribution definitions 
+dgumbel <- function(x, a, b) 1/b*exp((a-x)/b)*exp(-exp((a-x)/b))
+pgumbel <- function(q, a, b) exp(-exp((a-q)/b))
+qgumbel <- function(p, a, b) a-b*log(-log(p))
+
+fitgumbel <- fitdist(dat_nonmiss$m4, #replace with weighted var here if using survey weights
+                     "gumbel", start=list(a=1, b=1))
 summary(fitgumbel)
 plot(fitgumbel)
 
 bf_stats <- as.data.frame(fitgumbel$estimate)
 
 write.csv(bf_stats, file = "bf_stats.csv") #label with country name for saving
+
+mean(dat_nonmiss$m4)
