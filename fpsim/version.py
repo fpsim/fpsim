27c7cd38e1fed4c37b014e5c4babe8e93cb26dff
Annie Valente <65202227+avictorious@users.noreply.github.com>
2023-02-10 09:50:09 -0500

    update version number and changelog

--- fpsim/version.py
+++ fpsim/version.py
@@ -1,3 +1,3 @@
-__version__ = '0.22.0'
-__versiondate__ = '2023-1-24'
+__version__ = '0.23.0'
+__versiondate__ = '2023-2-10'
 __license__ = f'FPsim {__version__} ({__versiondate__}) — © 2019-2022 by IDM'
