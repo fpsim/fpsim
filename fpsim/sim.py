4da136bf3c51df8d1c3725f26dc9d6e4493a9e1b
nnoori-IDM <42287387+nnoori-IDM@users.noreply.github.com>
2023-02-28 07:56:43 -0800

    fix the title for the age specific short interval plotting round 3

--- fpsim/sim.py
+++ fpsim/sim.py
@@ -1664,7 +1664,7 @@ class Sim(fpb.BaseSim):
             elif to_plot == 'short-interval':
                 to_plot = {
 #                    'proportion_short_interval_by_year':     'Proportion of short birth interval'
-                    'proportion_short_interval_by_year':     f"Proportion of short birth interval ({age_group})" for age_group in agelim.split()
+                    'proportion_short_interval_by_year':     f"Proportion of short birth interval [{age_group})" for age_group in agelim.split()
                 }
             elif to_plot == 'as_cpr':
                 to_plot = {f"cpr_{age_group}": f"Contraceptive Prevalence Rate ({age_group})" for age_group in method_age_groups}
