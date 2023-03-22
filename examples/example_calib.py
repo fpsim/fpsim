38e2ac9cb158320888227317d6ded77b4505d77a
Annie Valente <65202227+avictorious@users.noreply.github.com>
2023-03-22 11:28:10 -0400

    fix example_calib and add plotting

--- examples/example_calib.py
+++ examples/example_calib.py
@@ -4,25 +4,39 @@ Simple example usage for FPsim
 
 import sciris as sc
 import fpsim as fp
-import fp_analyses.senegal_parameters as sp
+import pylab as pl
 
 # Settings
-doplot = True
-dosave = False
-pars = sp.make_pars()
+do_plot = True
+do_save = True
+pars = fp.pars(location='senegal')
 pars['n'] = 500
 pars['verbose'] = 0
 
 # Define calibration parameters -- best, low, high
+#calib_pars = dict(
+    #exposure_factor = [1.0, 0.5, 8.0],
+#)
+
 calib_pars = dict(
-    exposure_correction = [1.0, 0.5, 8.0],
+    high_parity=dict(best=4, low=1, high=8),
+    high_parity_nonuse=dict(best=0.5, low=0.1, high=10)
 )
 
 # Run
 T = sc.tic()
-calib = fp.Calibration(pars=pars, n_trials=10)
+calib = fp.Calibration(pars=pars, n_trials=100)
 calib.calibrate(calib_pars=calib_pars)
 calib.summarize()
+
+fig = calib.plot_best()
+if do_save:
+    pl.savefig("calib_best.png", bbox_inches='tight', dpi=100)
+
+fig = calib.plot_trend()
+if do_save:
+    pl.savefig("calib_trend.png", )
+
 sc.toc(T)
 
 print('Done.')
