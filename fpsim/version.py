586ff060df6e28e595475d3a9dedb828b8103440
Aasli <78984333+aaslinur@users.noreply.github.com>
2023-07-25 10:36:01 -0700

    Version number updates
    Updates changelog and version number for pull request #164

--- fpsim/version.py
+++ fpsim/version.py
@@ -1,4 +1,4 @@
-__version__ = '0.26.1'
-__versiondate__ = '2023-6-23'
+__version__ = '0.26.2'
+__versiondate__ = '2023-7-25'
 
 __license__ = f'FPsim {__version__} ({__versiondate__}) — © 2019-2022 by IDM'
