369c79de90f99f4caf735bbbeeb5bc7d09e9cb26
Paula Sanz-Leon <pmsl.academic@gmail.com>
2023-10-06 11:58:27 +1000

    Use 5 year bins for analysing empowerment metrics

--- examples/example_empowerment_microsim.py
+++ examples/example_empowerment_microsim.py
@@ -5,7 +5,7 @@ Simple simulation using the new empowement related attributes
 import sciris as sc
 import fpsim as fp
 import matplotlib.pyplot as plt
-
+import numpy as np
 # Set options
 do_plot = True
 pars = fp.pars(location='kenya')
@@ -14,7 +14,8 @@ pars['n_agents'] = 500 # Small population size
 
 
 sc.tic()
-sim = fp.Sim(pars=pars, analyzers=fp.empowerment_recorder(bins=[0, 15, 20, 40, 50, 100]))
+age_bins = np.arange(100)[::5] # 5 year bins
+sim = fp.Sim(pars=pars, analyzers=[fp.empowerment_recorder(bins=age_bins)])
 sim.run()
 
 if do_plot:
