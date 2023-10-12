da6d8253f6f19b3e01aa2e2c5e4d64a75596eb2f
Paula Sanz-Leon <pmsl.academic@gmail.com>
2023-10-12 12:06:36 +1000

    Write small sim with example on how plot education results
    Set fig size for edu trajectories

---
+++ examples/education_microsim.py
@@ -0,0 +1,29 @@
+'''
+Simple and small simulation using education analyzer
+'''
+
+import sciris as sc
+import fpsim as fp
+import matplotlib.pyplot as plt
+
+# Set options
+do_plot = True
+pars = fp.pars(location='kenya')
+pars['n_agents'] = 50 # Small population size
+
+
+
+sc.tic()
+sim = fp.Sim(pars=pars, analyzers=[fp.education_recorder()])
+sim.run()
+
+if do_plot:
+    edu_analyzer= sim.get_analyzers()[0]
+    #Plot a subset of the the available trajectories for female individuals
+    for idx in range(edu_analyzer.max_pop_size//8):
+        edu_analyzer.plot(index=idx)
+    edu_analyzer.plot_waterfall()
+    plt.show()
+
+sc.toc()
+print('Done.')
