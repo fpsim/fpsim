1b8b279eb5834f61231e0aac56a99125f038e7c1
Emily Claps <108016846+emilykathryn@users.noreply.github.com>
2023-11-21 13:07:21 -0800

    modified prediction.py to generate plots for mcpr, asfr, and method mix comparison

--- fpsim/locations/data_processing/prediction.py
+++ fpsim/locations/data_processing/prediction.py
@@ -2,19 +2,36 @@
 This is a script that runs a comparison between a country's newest data and the calibrated model's predicted values
 for that respective year. It can be used as a means of assessing a model's accuracy in its predictions.
 """
-
+import os
 import sciris as sc
 import fpsim as fp
 import numpy as np
+import pandas as pd
+import pylab as pl
+
 
 # Global Variables
 country = 'kenya'
+latest_year = 2022
 do_plot = True
+do_save = True
+min_age = 15
+max_age = 50
 
 # TODO: Import new data
 new_mcpr = 45.9
-# new_method_mix =
 new_asfr = [2.9, 62.6, 167.8, 171.6, 132.2, 77.0, 32.8, 11.7]
+new_method_mix = {
+    'Withdrawal': 1.04,
+    'Other traditional': 4.46,
+    'Condoms': 8.42,
+    'Pill': 7.95,
+    'Injectables': 34.07,
+    'Implants': 33.96,
+    'IUDs': 3.09,
+    'BTL': 3.88,
+    'Other modern': 3.13
+}
 # new_birth_spacing =
 
 age_bin_map = {
@@ -31,10 +48,61 @@ age_bin_map = {
 
 # Analyze difference between predicted and actual mcpr values. From UN Data Portal
 def compare_mcpr():
-    index = np.where(res['t'] == pars['end_year'])[0][0]
-    predicted_mcpr = res['mcpr'][index] * 100
+    res_index = np.where(res['t'] == pars['end_year'])[0][0]
+    predicted_mcpr = res['mcpr'][res_index] * 100
+    print('#################### MCPR COMPARISON ####################')
     print(f'Predicted mcpr value: {predicted_mcpr}')
     print(f'Actual mcpr value: {new_mcpr}')
+
+    # Set up plotting
+    df_mcpr = pd.DataFrame({'UN Data': new_mcpr, 'FPsim': predicted_mcpr}, index=['MCPR'])
+
+    # Plot use
+    ax = df_mcpr.plot.barh(color={'UN Data': 'black', 'FPsim': 'cornflowerblue'})
+    ax.set_xlabel('Percent')
+    ax.set_title(f'{country.capitalize()}: {latest_year} MCPR - Model vs Data')
+    if do_save:
+        pl.savefig(f"../{country}/prediction_figs/mcpr.png", bbox_inches='tight', dpi=100)
+
+    pl.show()
+
+    return
+
+
+# Analyze difference between predicted and actual asfr values. From UN Data Portal
+def compare_asfr():
+    x = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    x_labels = []
+    asfr_model = []
+
+    # Extract from model
+    for key in age_bin_map.keys():
+        x_labels.append(key)
+        asfr_model.append(res['asfr'][key][-1])
+
+    # Plot
+    fig, ax = pl.subplots()
+    kw = dict(lw=3, alpha=0.7, markersize=10)
+    ax.plot(x, new_asfr, marker='^', color='black', label="UN Data", **kw)
+    ax.plot(x, asfr_model, marker='*', color='cornflowerblue', label="FPsim", **kw)
+    pl.xticks(x, x_labels)
+    pl.ylim(bottom=-10)
+    ax.set_title(f'{country.capitalize()}: {latest_year} Age-specific fertility rate per 1000 woman years')
+    ax.set_xlabel('Age')
+    ax.set_ylabel(f'ASFR')
+    ax.legend(frameon=False)
+    sc.boxoff()
+
+    if do_save:
+        pl.savefig(f'../{country}/prediction_figs/asfr.png')
+
+    pl.show()
+
+    print('#################### ASFR COMPARISON ####################')
+    print(f'Predicted asfr value: {asfr_model}')
+    print(f'Actual asfr value: {new_asfr}')
+
     return
 
 
@@ -65,8 +133,8 @@ def compare_method_mix():
     model_method_counts = sc.odict().make(keys=model_labels_all, vals=0.0)
 
     # Extract from model
-    for i in range(len(sim.ppl)):
-        if ppl.alive[i] and not ppl.sex[i] and ppl.age[i] >= sim.pars['min_age'] and ppl.age[i] < sim.pars['min_age']:
+    for i in range(len(ppl)):
+        if ppl.alive[i] and not ppl.sex[i] and min_age <= ppl.age[i] < max_age:
             model_method_counts[ppl.method[i]] += 1
 
     model_method_counts[:] /= model_method_counts[:].sum()
@@ -79,28 +147,21 @@ def compare_method_mix():
     mix_model = model_methods_mix.values()[1:]
     mix_percent_model = [i * 100 for i in mix_model]
 
+    # Set up plotting
+    mix_percent_data = list(new_method_mix.values())
+    df_mix = pd.DataFrame({'PMA': mix_percent_data, 'FPsim': mix_percent_model}, index=model_labels_methods)
+
+    # Plot mix
+    ax = df_mix.plot.barh(color={'PMA': 'black', 'FPsim': 'cornflowerblue'})
+    ax.set_xlabel('Percent users')
+    ax.set_title(f'{country.capitalize()}: {latest_year} Contraceptive Method Mix - Model vs Data')
+    if do_save:
+        pl.savefig(f'../{country}/prediction_figs/method_mix.png', bbox_inches='tight', dpi=100)
+
+    pl.show()
+
     return
 
-# Analyze difference between predicted and actual asfr values. From UN Data Portal
-def compare_asfr():
-    # Print ASFR form model in output
-    for key in age_bin_map.keys():
-        print(f'ASFR (annual) for age bin {key} in the last year of the sim: {res["asfr"][key][-1]}')
-
-    x = [1, 2, 3, 4, 5, 6, 7, 8]
-
-    # Load data
-    year = data_asfr[data_asfr['year'] == pars['end_year']]
-    asfr_data = year.drop(['year'], axis=1).values.tolist()[0]
-
-    x_labels = []
-    asfr_model = []
-
-    # Extract from model
-    for key in age_bin_map.keys():
-        x_labels.append(key)
-        asfr_model.append(res['asfr'][key][-1])
-    return
 
 """
 # Analyze difference between predicted and actual birth spacing values. Generated from birth_spacing.R script
@@ -108,25 +169,32 @@ def compare_birth_spacing():
     return
 """
 
+
 if __name__ == "__main__":
-
+    # Set location and end_year sim parameters
     pars = fp.pars(location=country)
-    pars['end_year'] = 2022
+    pars['end_year'] = latest_year
 
+    # Run simulation
     sc.tic()
     sim = fp.Sim(pars=pars)
     sim.run()
 
+    # Save results and people to analyze
+    res = sim.results
     ppl = sim.people
 
+    # Generate and save plots, if configured
     if do_plot:
         sim.plot()
+        if do_save == 1 and os.path.exists(f'../{country}/prediction_figs') is False:
+            os.mkdir(f'../{country}/prediction_figs')
 
-    # Save results
-    res = sim.results
-
+    # Run comparison between model predicted values and new actual data
     compare_mcpr()
     compare_asfr()
+    compare_method_mix()
+    #compare_birth_spacing()
 
     sc.toc()
     print('Done.')
