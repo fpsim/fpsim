26ade1f74bdc69837613a4207f6f64b8ab41624f
Cliff Kerr <questionably@gmail.com>
2024-02-08 19:23:07 -0500

    Revert "remove timeseries_recorder and verbose_sim"
    This reverts commit 3e69e1a1f8dc8b69dc1dc5ef2287eaa5be373f53.

--- tests/test_analyzers.py
+++ tests/test_analyzers.py
@@ -66,6 +66,17 @@ def test_snapshot():
     return snap
 
 
+def test_timeseries_recorder():
+    sc.heading('Testing timeseries recorder...')
+
+    tsr = make_analyzer(fp.timeseries_recorder())
+
+    if do_plot:
+        tsr.plot()
+
+    return tsr
+
+
 def test_age_pyramids():
     sc.heading('Testing age pyramids...')
 
@@ -76,6 +87,87 @@ def test_age_pyramids():
 
     return ap
 
+def test_verbose_sim():
+    '''Test main verbose_sim functions'''
+    sc.heading('Testing verbose sim ...')
+    checked_channel = "gestation"
+    verbose_sim = make_analyzer(fp.verbose_sim())
+    verbose_sim.save(to_csv=True, to_json=True, custom_csv_tables=[checked_channel])
+    
+    total_results = verbose_sim.total_results
+    csv_filepath = f"sim_output/{checked_channel}_state.csv"
+    json_filepath = "sim_output/total_results.json"
+    
+    assert os.path.isfile(csv_filepath)
+    assert os.path.isfile(json_filepath)
+    
+    os.remove(csv_filepath)
+    os.remove(json_filepath)
+
+    ok('verbose_sim.save() succeeded')
+
+    assert all([len(channel) > 0 for channel in total_results[min(total_results.keys())]])
+    ok('no empty channels in verbose_sim')
+
+    verbose_sim.story(1)
+    ok('verbose_sim.story() succeeded')
+
+    return verbose_sim
+
+def test_verbose_channels():
+    ''' Test verbose_sim calculated channels '''
+    sc.heading('Testing verbose sim added channels...')
+    sim = fp.Sim(location='test', analyzers=fp.verbose_sim())
+    sim.run()
+    sim_verbose = sim['analyzers']
+    events = sim_verbose.events
+    channels =  sim_verbose.channels
+
+    # Checks that none of the channels from events contain no entries.
+    for channel in channels:
+        if channel != "Deaths":
+            maxval = 0
+            for timestep in events:
+                if len(events[timestep][channel]) > maxval:
+                    maxval = len(events[timestep][channel])
+
+        assert maxval > 0, f"Detected empty channel: {channel}"
+    ok('No empty channels')
+
+    miscarriages = 0
+    births = 0
+    conceptions = 0
+    deaths = 0
+
+    sim_births = 0
+    sim_deaths = 0
+
+    # Since analyzers are applied before people is updated for a timestep
+    # we will skip the last timestep
+    for timestep in events:
+        births += len(events[timestep]['Births'])
+        conceptions = conceptions + len(events[timestep]['Conceptions'])
+        miscarriages += len(events[timestep]['Miscarriages'])
+        deaths += len(events[timestep]['Deaths'])
+
+    sim_births = sum(sim.results['births'][:-1])
+    sim_deaths = sum(sim.results['deaths'][:-1])
+    assert births == sim_births, f"sim.results births is {sim_births} and births channel is {births} on timestep {timestep}"
+    assert deaths == sim_deaths, f"sim.results deaths is {sim_deaths} and deaths channel is {deaths} on timestep {timestep}"
+    
+    ok(f'No mismatch for births ({births} == {sim_births})')
+    ok(f'No mismatch for deaths ({deaths} == {sim_deaths})')
+
+    # Checks that conceptions is approximately births, and that conceptions is greater than the number of births.
+    # We wouldn't expect more than a quarter of conceptions to end in miscarriages
+    assert np.isclose(births, conceptions, atol=max_pregnancy_loss*births), "Less than 75 percent of conceptions result in births"
+    assert conceptions > births, "Number of conceptions not greater than recorded births"
+    ok(f'No mismatch for conceptions > miscarriages ({conceptions} > {miscarriages})')
+
+    # Checks that miscarriages < difference between conceptions and births
+    assert conceptions - births > miscarriages, "The number of miscarriages is greater than the differences between conceptions and births"
+    ok(f'No mismatch for conceptions - births > miscarriages ({conceptions} - {births} > {miscarriages})')
+
 
 if __name__ == '__main__':
 
@@ -83,4 +175,7 @@ if __name__ == '__main__':
     with sc.timer():
         calib = test_calibration()
         snap  = test_snapshot()
+        tsr   = test_timeseries_recorder()
         ap    = test_age_pyramids()
+        vs    = test_verbose_sim()
+        nc    = test_verbose_channels()
