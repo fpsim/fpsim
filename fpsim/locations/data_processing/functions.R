f21235df86d578a6934874d109b7c1970ce84d08
mzimmermann-IDM <48844053+mzimmermann-IDM@users.noreply.github.com>
2024-02-15 11:40:18 -0800

    empowerment functions

---
+++ fpsim/locations/data_processing/functions.R
@@ -0,0 +1,64 @@
+#############################################
+# -- Longitudinal analysis on PMA data
+# -- For empowerment implementation in FPsim
+# -- Marita Zimmermann
+# -- October 2023
+#############################################
+
+rm(list=ls())
+
+library(lavaan)
+library(tidySEM)
+library(ggpubr)
+library(extrafont)
+library(ggalluvial)
+library(survey)
+library(lavaan.survey)
+library(tidyverse)      
+library(haven)  
+library(withr)  
+
+pma.path <- normalizePath(file.path(Sys.getenv("ONEDRIVE"), "WRICH/Data/PMA"), "/")
+All_data_wide_clpm <- with_dir(pma.path, {read.csv("Kenya/All_data_wide_clpm.csv")})
+options(survey.lonely.psu="adjust")
+
+
+
+####################################################################################################################################
+
+filter_data <- All_data_wide_clpm %>%
+  filter(!is.na(EA_ID)) %>%
+  # refuse sex only waves 1 and 3, so set wave 2 = wave 1 but remember that coefficient is for 2 years prior
+  mutate(wge_sex_eff_tell_no_2 = wge_sex_eff_tell_no_3)
+
+svydes <- svydesign(id = ~EA_ID, strata = ~strata, weights =  ~FQweight, data = filter_data, nest = T)
+other.outcomes <- c("paidw_12m", "decide_spending_mine", "buy_decision_health", "wge_sex_eff_tell_no")
+      
+# Empowerment
+empower_results <- list()
+modellist <- list()
+for (i in other.outcomes) {
+  print(i)
+  model <- svyglm(as.formula(paste0(i,"_3 ~ current_contra_3 + ",i,"_2  + age_3 + married_3 + school_3 + live_births_3 + urban_3")), 
+                  family = quasibinomial(), design = svydes)
+  modellist[[i]] <- model
+  empower_results[[i]] <- as.data.frame(summary(model)$coefficients) %>% 
+    mutate(lhs = i, rhs = rownames(.)) }
+empower_coef <- bind_rows(empower_results)  %>%
+  # couldn't converge for sexual autonomy so replace
+  filter(lhs != "wge_sex_eff_tell_no") %>%
+  bind_rows(data.frame(lhs = "wge_sex_eff_tell_no", rhs = unique(empower_results$wge_sex_eff_tell_no$rhs)) %>% mutate(Estimate = ifelse(rhs == "wge_sex_eff_tell_no_2", 1, 0))) %>%
+  # rename variables to match model
+  mutate(across(c(lhs, rhs), ~gsub("_3", "", gsub("_2", "_0", 
+                                                  gsub("school","edu_attainment",
+                                                       gsub("paidw_12m","paid_employment",
+                                                            gsub("decide_spending_mine","decision_wages",
+                                                                 gsub("buy_decision_health","decision_health",
+                                                                      gsub("wge_sex_eff_tell_no","sexual_autonomy",
+                                                                           gsub("married","partnered",
+                                                                                gsub("live_births", "parity", .)))))))))))
+
+write.csv(empower_coef, "fpsim/locations/kenya/empower_coef.csv", row.names = F)
+
+
+
