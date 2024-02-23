fdc79685dc40275f26752beb28fb802fd6489b1e
Robyn Stuart <robyna.s@gmail.com>
2024-02-23 13:26:07 +1100

    benchmarking

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -9,7 +9,9 @@ do_profile = 1
 sim = fp.Sim()
 sim.initialize()
 
+ms = fp.MethodSelector(contra_use_file='contra_coef.csv', method_choice_file='method_mix.csv')
 ppl = sim.people
+
 to_profile = sc.objdict(
     run         = sim.run,
     update      = ppl.update,           # 70% of sim.run() runtime is spent here
@@ -17,13 +19,15 @@ to_profile = sc.objdict(
     methods     = ppl.update_methods,   # 50% of ppl.update() runtime is spent here
     method_pp   = ppl.update_method_pp, # 53% of ppl.update_methods() runtime is spent here
     method      = ppl.update_method,    # 44% of ppl.update_methods() runtime is spent here
+    choose_method = ms.choose_method,    # 44% of ppl.update_methods() runtime is spent here
     filter      = ppl.filter            # 58% of ppl.update_method() runtime is spent here
-)['run']
+)['choose_method']
 
 
 def run():
-    pars = fp.pars(n_agents=10e1, method_timestep=1, verbose=0)
-    sim = fp.Sim(pars)
+    ms = fp.MethodSelector(contra_use_file='contra_coef.csv', method_choice_file='method_mix.csv')
+    pars = fp.pars(location='test', verbose=1)
+    sim = fp.Sim(pars, method_selector=ms)
     sim.run()
     return sim
 
