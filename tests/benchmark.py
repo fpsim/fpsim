ae5d8a21b01b907f32b5a94c3e9c3cb30e390ac5
Robyn Stuart <robyna.s@gmail.com>
2024-03-08 07:34:32 +1100

    work on benchmark scripts

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -9,25 +9,25 @@ do_profile = 1
 sim = fp.Sim()
 sim.initialize()
 
-ms = fp.MethodSelector(contra_use_file='contra_coef.csv', method_choice_file='method_mix.csv')
+coefficients = sc.objdict(intercept=.1, age=2, parity=3)
+method_choice = fp.SimpleChoice(coefficients)
 ppl = sim.people
 
 to_profile = sc.objdict(
     run         = sim.run,
     update      = ppl.update,           # 70% of sim.run() runtime is spent here
     people_init = ppl.__init__,
-    methods     = ppl.update_methods,   # 50% of ppl.update() runtime is spent here
-    method_pp   = ppl.update_method_pp, # 53% of ppl.update_methods() runtime is spent here
     method      = ppl.update_method,    # 44% of ppl.update_methods() runtime is spent here
-    choose_method = ms.choose_method,    # 44% of ppl.update_methods() runtime is spent here
+    choose_method = method_choice.choose_method,    # 44% of ppl.update_methods() runtime is spent here
     filter      = ppl.filter            # 58% of ppl.update_method() runtime is spent here
 )['choose_method']
 
 
 def run():
-    ms = fp.MethodSelector(contra_use_file='contra_coef.csv', method_choice_file='method_mix.csv')
+    coefficients = sc.objdict(intercept=.1, age=2, parity=3)
+    method_choice = fp.SimpleChoice(coefficients)
     pars = fp.pars(location='test', verbose=1)
-    sim = fp.Sim(pars, method_selector=ms)
+    sim = fp.Sim(pars, contraception_module=method_choice)
     sim.run()
     return sim
 
--- tests/benchmark_empowerment.py
+++ tests/benchmark_empowerment.py
@@ -8,8 +8,11 @@ import fpsim as fp
 import fpsim.education as fpemp
 
 
-do_profile = 0
-sim = fp.Sim()
+do_profile = 1
+ms = fp.EmpoweredChoice(location='kenya')
+emp = fp.Empowerment(location='kenya')
+edu = fp.Education(location='kenya')
+sim = fp.Sim(contraception_module=ms, empowerment_module=emp, education_module=edu)
 sim.initialize()
 
 ppl = sim.people
@@ -17,32 +20,35 @@ to_profile = sc.objdict(
     run                 =   sim.run,
     ppl_update    =  ppl.update,
     ppl_init      =  ppl.__init__,
-    education =  fpemp.update_education,
-    edu_start     =  fpemp.start_education,             # 12% of update_education()
-    edu_advance   =  fpemp.advance_education,    # 56% of update_education()
-    edu_interrupt =  fpemp.interrupt_education,  # 19% of advance_education()
-    edu_dropout   =  fpemp.dropout_education,    # 30% of advance_education() -- called twice
-    edu_resume    =  fpemp.resume_education,     # 16% of update_education()
-    edu_done      =  fpemp.graduate,             # 14% of update_education()
+    education     =  edu.update,
+    edu_start     =  edu.start_education,      # 12% of update_education()
+    edu_advance   =  edu.advance_education,    # 56% of update_education()
+    edu_interrupt =  edu.interrupt_education,  # 19% of advance_education()
+    edu_dropout   =  edu.dropout_education,    # 30% of advance_education() -- called twice
+    edu_resume    =  edu.resume_education,     # 16% of update_education()
+    edu_done      =  edu.graduate,             # 14% of update_education()
+    empowerment   =  emp.update,
 )
 
 
 def run_with_empowerment():
-    pars = fp.pars(location='kenya', n_agents=10e1, method_timestep=1, verbose=0, use_empowerment=True)
-    sim = fp.Sim(pars)
+    pars = fp.pars(location='kenya', n_agents=10e1, verbose=0)
+    sim = fp.Sim(pars, contraception_module=ms, empowerment_module=emp, education_module=edu)
     sim.run()
     return sim
 
 
 def run_without_empowerment():
-    pars = fp.pars(location='kenya', n_agents=10e1, method_timestep=1, verbose=0, use_empowerment=False)
-    sim = fp.Sim(pars)
+    pars = fp.pars(location='kenya', n_agents=10e1, verbose=0)
+    coefficients = sc.objdict(intercept=.1, age=2, parity=3)
+    ms = fp.SimpleChoice(coefficients)
+    sim = fp.Sim(pars, contraception_module=ms)
     sim.run()
     return sim
 
 
 if __name__ == '__main__':
-    selection = 'education'
+    selection = 'ppl_update'
     if do_profile:
         sc.profile(run_with_empowerment, to_profile[selection])
         sc.profile(run_without_empowerment, to_profile[selection])
