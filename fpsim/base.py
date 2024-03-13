0a2c53b24c86b5ef7bb87541a957feae9359ec82
Robyn Stuart <robyna.s@gmail.com>
2024-03-13 10:45:17 +0100

    cleanup base

--- fpsim/base.py
+++ fpsim/base.py
@@ -182,18 +182,8 @@ class BasePeople(sc.prettyobj):
     def __setattr__(self, attr, value):
         ''' Ditto '''
         if self._is_filtered(attr):
-            arr = obj_get(self, attr)
-
-            if arr.ndim == 1:
-                arr[self.inds] = value
-            elif arr.ndim == 2:
-                import traceback; traceback.print_exc(); import pdb; pdb.set_trace()
-
-                arr[self.inds, :] = value
-            else:
-                errormsg = 'Can only operate on 1D or 2D arrays'
-                raise TypeError(errormsg)
-
+            array = obj_get(self, attr)
+            array[self.inds] = value
         else:   # If not filtered, just set
             obj_set(self, attr, value)
         return
