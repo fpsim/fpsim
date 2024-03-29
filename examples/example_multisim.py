b265d67029312e03eade24e91404146200b7fc97
Emily Claps <108016846+emilykathryn@users.noreply.github.com>
2024-03-28 18:08:44 -0700

    added simple example of multisim that will be the baseline of the msim used for ethiopia regions

---
+++ examples/example_multisim.py
@@ -0,0 +1,36 @@
+'''
+Simple example usage for FPsim multisim
+'''
+
+import sciris as sc
+import fpsim as fp
+
+# Set options
+do_plot = True
+pars1 = fp.pars(location='amhara')
+pars1['n_agents'] = 500 # Small population size
+pars1['end_year'] = 2020 # 1961 - 2020 is the normal date range
+pars1['exposure_correction'] = 1.0 # Overall scale factor on probability of becoming pregnant
+
+pars2 = fp.pars(location='somali')
+pars2['n_agents'] = 500 # Small population size
+pars2['end_year'] = 2020 # 1961 - 2020 is the normal date range
+pars2['exposure_correction'] = 1.0 # Overall scale factor on probability of becoming pregnant
+
+
+if __name__ == '__main__':
+    sc.tic()
+
+    sim1 = fp.Sim(pars=pars1)
+    sim2 = fp.Sim(pars=pars2)
+
+    msim = fp.MultiSim(sims=[sim1, sim2])
+
+    msim.run()
+    mm = msim.merge()
+
+    if do_plot:
+        msim.plot()
+
+    sc.toc()
+    print('Done.')
