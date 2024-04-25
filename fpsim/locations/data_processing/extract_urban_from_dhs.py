d54071593c0249541f213e87db276ec11865e4a0
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-04-25 12:06:56 +1000

    Use weights to estimate urban proportion

--- fpsim/locations/data_processing/extract_urban_from_dhs.py
+++ fpsim/locations/data_processing/extract_urban_from_dhs.py
@@ -5,40 +5,57 @@ from a DHS DTA file, Individual Women's Data - Individual Recode (IR).
 
 This dataset has one record for every eligible woman
 as defined by the household schedule.
+
+NOTE: The Python ecosystem does not seem to have an equivalent to R's survey package.
+Thus, results from the calculations below should be used with a clear understanding
+of their statistical limitations as we do not perform the full range of survey
+adjustments as carried out by svydesign in R.
 """
+
+import pathlib
 
 import pandas as pd
 import sciris as sc
-import pathlib
+
 import fpsim.locations as fplocs
 
 # Assumes datasets are stored in /home/user/DHS/...
 # Replace with relevant directories and filenames
 home_dir = pathlib.Path.home()
 dhs_dir = "DHS"
-country_dir = "KENYA"
+country_dir = "KEIR8BDT"
 filename = "KEIR8BFL.DTA"
 
 # Path to DHS dataset stata file (.DTA)
 filepath = sc.path(home_dir, dhs_dir, country_dir, filename)
 data_raw = pd.read_stata(filepath, convert_categoricals=False)
 
+
+cols_to_keep = ["v001", "v005", "v023", "v025"]
+urban_dhs_var = "v025"  # DHS-8 variable name about type of place of residence
+
+data = data_raw[cols_to_keep].copy()
+
+#Take the 'women's individual sample weight' variable and divide by 1 million.
+data["sample_weight"] = data["v005"]/1e6
+data["urban"] = (data[urban_dhs_var] == 1).astype(float)
+data["weighted_urban"] = data["urban"].multiply(data['sample_weight'], axis=0)
+
 # Process the necessary information
-urban_var = "v025"  # DHS-8 variable name about type of place of residence
-data_processed = pd.DataFrame(columns=["mean", "urban.se"])
-urban = (data_raw[urban_var] == 1).astype(float)
-data_processed["mean"] = urban.mean()
-data_processed["urban.se"] = urban.sem()
+data_processed = pd.DataFrame({
+    "mean": [data["weighted_urban"].mean()],
+    "urban.se": [data["weighted_urban"].sem()]
+})
+
 
 # If you want to write this DataFrame to a .csv file, in
 # the corresponding locations folder you can do:
 locations_path = sc.thisdir(fplocs)
 country = "kenya"
-output_filename = "urban.csv"
+output_filename = "urban_.csv"
 
 # Actually save the data
+# NOTE that this will replace the default urban.csv
 data_processed.to_csv(sc.path(locations_path, country,
                               output_filename),
                       index=False)
-# NOTE that this will replace the default urban.csv
-# TODO: maybe we automatically save a backup of default urban.csv?
