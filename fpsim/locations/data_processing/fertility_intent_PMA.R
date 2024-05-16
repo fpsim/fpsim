f98c401894a1a2706a238db6a93c9e79d42f3cc1
Marita <mzimmermann@idmod.org>
2024-05-16 07:39:56 -0700

    Fertility intent

---
+++ fpsim/locations/data_processing/fertility_intent_PMA.R
@@ -0,0 +1,20 @@
+###################################################
+# -- Analysis on PMA data
+# -- Find distribution of fertility intent by age
+# -- Marita Zimmermann
+# -- May 2024
+###################################################
+
+# -- To get formatted PMA dataset, run PMA_recode in data processing folder
+
+library(tidyverse)      
+library(survey)
+
+
+options(survey.lonely.psu="adjust")
+svydes <- svydesign(id = ~EA_ID, strata = ~strata, weights =  ~FQweight, data = All_data , nest = T)
+
+fertility_intent <- as.data.frame(prop.table(svytable(~fertility_intent+age, svydes), 2)) %>%
+  filter(fertility_intent == 1) %>% select(-fertility_intent)
+
+write.csv(fertility_intent, "fpsim/locations/data_processing/fertility_intent.csv", row.names = F)
