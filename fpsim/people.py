af34ce7e1a146a960c680c67b32a0eb1c30d4d3b
Robyn Stuart <robyna.s@gmail.com>
2024-06-04 16:00:12 +1000

    add back tests

--- fpsim/people.py
+++ fpsim/people.py
@@ -162,20 +162,17 @@ class People(fpb.BasePeople):
                 # Get previous users and see whether they will switch methods or stop using
                 prev_users = self.filter(self.on_contra)
                 if len(prev_users):
-                    if self.ti==14:
-                        print('hi')
-
                     prev_users.on_contra = cm.get_contra_users(prev_users)
 
-                # For those who keep using, determine their next method and update time
-                still_on_contra = prev_users.filter(prev_users.on_contra)
-                still_on_contra.method = cm.choose_method(still_on_contra)
-                still_on_contra.ti_contra = cm.set_dur_method(still_on_contra)
+                    # For those who keep using, determine their next method and update time
+                    still_on_contra = prev_users.filter(prev_users.on_contra)
+                    still_on_contra.method = cm.choose_method(still_on_contra)
+                    still_on_contra.ti_contra = cm.set_dur_method(still_on_contra)
 
-                # For those who stop using, determine when next to update
-                stopping_contra = prev_users.filter(~prev_users.on_contra)
-                stopping_contra.method = 0
-                stopping_contra.ti_contra = cm.set_dur_method(stopping_contra)
+                    # For those who stop using, determine when next to update
+                    stopping_contra = prev_users.filter(~prev_users.on_contra)
+                    stopping_contra.method = 0
+                    stopping_contra.ti_contra = cm.set_dur_method(stopping_contra)
 
                 # Validate
                 n_methods = len(self.contraception_module.methods)
@@ -187,7 +184,7 @@ class People(fpb.BasePeople):
             if event in ['pp1', 'pp6']:
                 self.on_contra = cm.get_contra_users(self, event=event)
                 on_contra = self.filter(self.on_contra)
-                on_contra.method = cm.choose_method(on_contra)
+                on_contra.method = cm.choose_method(on_contra, event=event)
                 on_contra.ti_contra = cm.set_dur_method(on_contra)
 
         return
