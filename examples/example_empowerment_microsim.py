36927f1f60785831869762d15a44bbe599383cde
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-06-13 19:19:45 +1000

    Use empowerment module in empwr example sim

--- examples/example_empowerment_microsim.py
+++ examples/example_empowerment_microsim.py
@@ -1,20 +1,21 @@
 '''
 Simple simulation using the new empowement related attributes
 '''
+import numpy as np
+import matplotlib.pyplot as plt
 
 import sciris as sc
 import fpsim as fp
-import matplotlib.pyplot as plt
-import numpy as np
+
 # Set options
 do_plot = True
-edu = fp.Education()
+empwr = fp.Empowerment()
 pars = fp.pars(location='kenya')
 pars['n_agents'] = 500  # Small population size
 
 sc.tic()
 age_bins = np.arange(100)[::5] # 5 year bins
-sim = fp.Sim(pars=pars, education_module=edu, analyzers=[fp.empowerment_recorder(bins=age_bins)])
+sim = fp.Sim(pars=pars, empowerment_module=empwr, analyzers=[fp.empowerment_recorder(bins=age_bins)])
 sim.run()
 
 if do_plot:
