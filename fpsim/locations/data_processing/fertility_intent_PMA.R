0a2406f497278aaa25efdbd14c6b38721a900cc6
Marita <mzimmermann@idmod.org>
2024-06-20 11:43:28 -0700

    contraceptive intent file

--- fpsim/locations/data_processing/fertility_intent_PMA.R
+++ fpsim/locations/data_processing/fertility_intent_PMA.R
@@ -1,6 +1,7 @@
 ###################################################
 # -- Analysis on PMA data
 # -- Find distribution of fertility intent by age
+# -- Find distibution of contraceptive intent by age
 # -- Marita Zimmermann
 # -- May 2024
 ###################################################
@@ -14,6 +15,11 @@ library(survey)
 options(survey.lonely.psu="adjust")
 svydes <- svydesign(id = ~EA_ID, strata = ~strata, weights =  ~FQweight, data = All_data , nest = T)
 
-fertility_intent <- as.data.frame(prop.table(svytable(~fertility_intent+age, svydes), 2)) 
 
+# Fertility intent by age
+fertility_intent <- as.data.frame(prop.table(svytable(~fertility_intent+age, svydes), 2)) 
 write.csv(fertility_intent, "fpsim/locations/kenya/fertility_intent.csv", row.names = F)
+
+# Contraceptive intent by age, among non-users
+contra_intent <- as.data.frame(prop.table(svytable(~intent_contra+age, svydes), 2)) 
+write.csv(contra_intent, "fpsim/locations/kenya/contra_intent.csv", row.names = F)
