c49365bd55cd0ba1b8efa81f187d83c86b6896e3
Robyn Stuart <robyna.s@gmail.com>
2024-07-19 08:14:03 +1000

    remove from tests

--- tests/test_interventions.py
+++ tests/test_interventions.py
@@ -47,35 +47,15 @@ def test_change_par():
     s0 = make_sim(label='Baseline')
     s1 = make_sim(interventions=cp1, label='High exposure')
 
-    # Define MCPR growth test
-    sim_start = 2015
-    mcpr_y1   = 2020 # When the MCPR projection starts
-    mcpr_y2   = 2030 # Max MCPR
-    mcpr_y3   = 2038 # MCPR should've declined
-    sim_end   = 2040 # Reset at this point
-    cp2 = fp.change_par('mcpr_growth_rate', vals={mcpr_y1:0.10, mcpr_y2:-0.10, mcpr_y3:'reset'}, verbose=verbose) # Set to large positive then negative growth
-    s2 = make_sim(interventions=cp2, start_year=sim_start, end_year=sim_end, label='Changing MCPR')
-
     # Run
-    m = fp.parallel(s0, s1, s2, serial=serial, compute_stats=False)
-    s0, s1, s2 = m.sims[:] # Replace with run versions
+    m = fp.parallel(s0, s1, serial=serial, compute_stats=False)
+    s0, s1, = m.sims[:] # Replace with run versions
 
     # Test exposure factor change
     base_births = s0.results['births'].sum()
     cp_births   = s1.results['births'].sum()
     assert s1['exposure_factor'] == ec, f'change_pars() did not change exposure factor to {ec}'
     assert cp_births < base_births, f'Reducing exposure factor should reduce births, but {cp_births} is not less than the baseline of {base_births}'
-
-    ## RS NOTE: Commenting out this test as it won't work with the new methodtime implementation
-    # # Test MCPR growth
-    # r = s2.results
-    # ind_y1 = sc.findnearest(r.t, mcpr_y1)
-    # ind_y2 = sc.findnearest(r.t, mcpr_y2)
-    # ind_y3 = sc.findnearest(r.t, mcpr_y3)
-    # assert ind_y1 != ind_y2 != ind_y3, f'Sim indices for years years {mcpr_y1}, {mcpr_y2}, {sim_end} should be different'
-    # assert r.mcpr[ind_y1] < r.mcpr[ind_y2], f'MCPR did not grow from {mcpr_y1} to {mcpr_y2}'
-    # assert r.mcpr[ind_y3] < r.mcpr[ind_y2], f'MCPR did not shrink from {mcpr_y2} to {sim_end}'
-    # assert s2['mcpr_growth_rate'] == s0['mcpr_growth_rate'], 'MCPR growth rate did not reset correctly'
 
     # Check user input validation
     with pytest.raises(ValueError): # Check that length of years and values match
