b10205c6c725ff1ad9386da25d1aa7afe19fdf0a
Robyn Stuart <robyna.s@gmail.com>
2024-07-30 14:23:46 +1000

    remove plot

--- examples/calibration_scripts/calibrate_simple.py
+++ examples/calibration_scripts/calibrate_simple.py
@@ -48,7 +48,7 @@ do_plot_ageparity = False
 do_plot_cpr = True
 do_plot_tfr = True
 do_plot_pop_growth = False
-do_plot_birth_space_afb = True
+do_plot_birth_space_afb = False
 do_plot_contra_analysis = False
 
 # Set option to save figures
