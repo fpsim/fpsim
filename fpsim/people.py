7c50f3e514ac068d8bb882324a17b15e0d349f4f
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-08-01 13:22:41 +1000

    Implment #242: intent to use initialisation (in People) and updates based on regression model (in Empowerment module)

--- fpsim/people.py
+++ fpsim/people.py
@@ -972,6 +972,7 @@ class People(fpb.BasePeople):
             # Update states on her bday, based on coefficients
             bday = eligible.birthday_filter()
             if len(bday):
+                # The empowerment module will update the empowerment states and intent to use
                 self.empowerment_module.update(bday)
                 # Update fertility intent on her bday, together with empowerment updates
                 bday.update_fertility_intent_by_age()
