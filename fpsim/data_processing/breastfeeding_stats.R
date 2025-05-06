64b8a675eaf07aa1dce89caba6587c66b0359278
Marita <mzimmermann@idmod.org>
2025-05-06 15:47:48 -0700

    Update bf duration code

--- fpsim/data_processing/breastfeeding_stats.R
+++ fpsim/data_processing/breastfeeding_stats.R
@@ -1,30 +1,48 @@
 ### Data to process DHS data for breastfeeding duration
 
+library(tidyverse)
+library(survey)
+library(fitdistrplus)
+library(truncdist)
+
+# Get DHS data
 dir <- "directory" ## directory with data
-filename <- "filename" ## must be a KR (child recode) file from the DHS for automatic processing
+filename <- "filename" ## must be a IR (individual recode) file from the DHS for automatic processing
 
-##Breastfeeding duration 
 setwd(dir)
-dat <- read.dta13(filename)
+dat <- readstata13::read.dta13(filename)
 
-require(fitdistrplus)
+# Data cleaning
+dat_nonmiss <- dat %>%
+  # select categories of breastfeeding, months breastfed, age of baby in months, and survey variables
+  select(starts_with("m4_"), starts_with("b19_"), v005, v021, v023, caseid) %>% 
+  # make long format with a row for each birth
+  gather(var, val, -v005, -v021, -v023, -caseid) %>% separate(var, into = c("var", "birth")) %>% mutate(birth = as.numeric(birth)) %>% spread(var, val) %>% 
+  mutate(age_months = as.numeric(b19), # ensure age is numeric
+         wt = v005/1e6, # calucalte survey weight
+         still_bf = ifelse(m4 == "still breastfeeding", 1, 0), # create indicator for still breastfeeding
+         age_grp = floor(age_months / 2) * 2) %>% # 2 month age bins (it's too noisy with individual age bins)
+  filter(age_months < 36 & !is.na(m4))  # include babies less than 3 years old and non-missing data
 
-dat_nonmiss <- subset(dat, m4 < 93) #m5 = months of breastfeeding in previous pregnancy
-dat_nonmiss$m4_weighted <- (dat_nonmiss$v005/1000000) * dat_nonmiss$m4 #use this code to apply survey weights
+
+# survey weighted table of proportion still breastfeeding of that age group  
+summary_table <- svyby(~still_bf, ~age_grp, 
+                       svydesign(id=~v021, weights=~wt, strata=~v023, data = dat_nonmiss), 
+                       FUN = svymean) %>%
+  arrange(age_grp) %>% mutate(p_stop = lag(still_bf) - still_bf,  # estimate of stopping probability, basically the PDF
+                              counts = ifelse(p_stop<0, 0, round(p_stop*10000))) %>% # create pseudo-counts to simulate a sample size, change and negatives to 0
+  filter(!is.na(p_stop)) # remove month 0 
 
 
-#gumbel distribution definitions 
-dgumbel <- function(x, a, b) 1/b*exp((a-x)/b)*exp(-exp((a-x)/b))
-pgumbel <- function(q, a, b) exp(-exp((a-q)/b))
-qgumbel <- function(p, a, b) a-b*log(-log(p))
+# Create a vector of repeated ages
+ages_rep <- rep(summary_table$age_grp, summary_table$counts)
 
-fitgumbel <- fitdist(dat_nonmiss$m4, #replace with weighted var here if using survey weights
-                     "gumbel", start=list(a=1, b=1))
-summary(fitgumbel)
-plot(fitgumbel)
+# Fit truncated normal distribution
+fit_tnorm <- fitdist(ages_rep, "norm", start = list(mean = mean(ages_rep), sd = sd(ages_rep)))
+summary(fit_tnorm)
 
-bf_stats <- as.data.frame(fitgumbel$estimate)
+# Save estimates in a csv in relevant country folder
+estimates <- as.data.frame(fit_tnorm$estimate)
+write.csv(estimates, file = "bf_stats.csv")
 
-write.csv(bf_stats, file = "bf_stats.csv") #label with country name for saving
 
-mean(dat_nonmiss$m4)
