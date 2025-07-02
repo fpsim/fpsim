417f23b6a7b0d4294017a247e61c36ed6b44397d
Robyn Stuart <robyna.s@gmail.com>
2025-07-03 07:06:23 +1000

    cleanup

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -53,6 +53,6 @@ def test_mid_choice():
 if __name__ == '__main__':
 
     s0 = test_simple('ethiopia')
-    # sims1 = test_simple_choice()
-    # sims2 = test_mid_choice()
+    sims1 = test_simple_choice()
+    sims2 = test_mid_choice()
     print('Done.')
