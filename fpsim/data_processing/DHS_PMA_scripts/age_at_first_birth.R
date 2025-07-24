04a392b8b32633d1848a25642ebb8185bfff4119
lrkbruce <lauryn.keeler.bruce@gmail.com>
2025-07-23 17:10:56 -0700

    Added option for region filtering for DHS data processing

--- fpsim/data_processing/DHS_PMA_scripts/age_at_first_birth.R
+++ fpsim/data_processing/DHS_PMA_scripts/age_at_first_birth.R
@@ -37,13 +37,27 @@ for (pkg in required_packages) {
 # - v212: age at first birth
 # - v201: number of children ever born
 # - v012: current age
+# - v024: region
 
-dhs_data <- read_dta(dhs_path,
-                     col_select = c("v005", "caseid", "v212", "v201", "v012")) %>%
+# Filter if region and region_code are defined
+if (exists("region_variable") && exists("region") && exists("region_code")) {
+  dhs_data <- read_dta(dhs_path,
+                     col_select = c("v005", "caseid", "v212", "v201", "v012", region_variable)) 
+  dhs_data <- dhs_data %>% 
+    filter(.data[[region_variable]] == region_code)
+} else {
+  dhs_data <- read_dta(dhs_path,
+                     col_select = c("v005", "caseid", "v212", "v201", "v012")) 
+}
+
+
+# Apply mutate after optional filtering
+dhs_data <- dhs_data %>%
   mutate(
     wt = v005 / 1e6,
     afb = ifelse(v201 == 0, Inf, v212)  # Assign Inf for those with no children
   )
+print(dhs_data)
 
 # -------------------------------
 # 3. Prepare and Save Output
@@ -54,7 +68,12 @@ afb_table <- dhs_data %>%
   dplyr::select(wt, age = v012, afb)
 
 # Create country-based output directory if it doesn't exist
-output_dir <- file.path(output_dir, country, 'data')
+if (exists("region") && exists("region_code")) {
+  output_dir <- file.path(output_dir, paste0(country, "_", region), 'data')
+} else {
+  output_dir <- file.path(output_dir, country, 'data')
+}
+
 if (!dir.exists(output_dir)) {
   dir.create(output_dir, recursive = TRUE)
 }
