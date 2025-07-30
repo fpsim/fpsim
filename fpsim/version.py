0640943aa25e6f441152d3b872ab0b397d838e10
emilydriano <emily.driano@gatesfoundation.org>
2025-07-30 11:45:37 -0700

    updated changelog and version

--- fpsim/version.py
+++ fpsim/version.py
@@ -1,3 +1,3 @@
-__version__ = '3.0.3'
-__versiondate__ = '2025-07-09'
+__version__ = '3.0.4'
+__versiondate__ = '2025-07-30'
 __license__ = f'FPsim {__version__} ({__versiondate__}) — © 2019-2025 by IDM'
