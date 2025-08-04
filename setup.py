41755157f79f94eee2a53bcd6a691ff5f0504eea
Cliff Kerr <questionably@gmail.com>
2025-08-04 18:18:54 -0400

    switch from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,53 +1,6 @@
-import os
-import runpy
-from setuptools import setup, find_packages
+"""
+Legacy support; see pyproject.toml for current information
+"""
+import setuptools
 
-# Get version
-cwd = os.path.abspath(os.path.dirname(__file__))
-versionpath = os.path.join(cwd, 'fpsim', 'version.py')
-version = runpy.run_path(versionpath)['__version__']
-
-# Get the documentation
-with open(os.path.join(cwd, 'README.rst'), "r") as f:
-    long_description = f.read()
-
-CLASSIFIERS = [
-    "Environment :: Console",
-    "Intended Audience :: Science/Research",
-    "License :: Other/Proprietary License",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    "Development Status :: 5 - Production/Stable",
-    "Programming Language :: Python :: 3.9",
-]
-
-setup(
-    name="fpsim",
-    version=version,
-    author="Michelle O'Brien, Annie Valente, Cliff Kerr, Sam Buxton, Daniel Klein, Marita Zimmermann, Emily Driano",
-    author_email="info@fpsim.org",
-    description="FPsim: Family Planning Simulator",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    url='http://fpsim.org',
-    keywords=["family planning", "women's health", "agent-based model", "simulation"],
-    platforms=["OS Independent"],
-    classifiers=CLASSIFIERS,
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'numpy',
-        'numba',
-        'scipy',
-        'starsim==2.3.1',
-        'pandas>=1.3', 
-        'sciris>=2.1.0',
-        'matplotlib>=3.5.0',
-        'seaborn>=0.9',
-        'optuna',
-        'plotnine',
-        'pyarrow',
-        'pyyaml',
-    ],
-)
+setuptools.setup()
