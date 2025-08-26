4ae3603aa74a0482795f3b15f08ca0415284426f
Robyn Stuart <robyna.s@gmail.com>
2025-08-26 15:20:16 +1000

    update dependencies

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'numpy',
         'numba',
         'scipy',
-        'starsim',
+        'starsim>=3.0.2',
         'pandas>=1.3', 
         'sciris>=2.1.0',
         'matplotlib>=3.5.0',
