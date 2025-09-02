6c175c3831967125000512684ef2b632d35a2124
Robyn Stuart <robyna.s@gmail.com>
2025-09-02 16:25:55 +1000

    need to decide what to do about unused data

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -68,30 +68,30 @@ def test_sim_creation():
     # Test 1: par passed in separate dicts
     contra_pars = dict(prob_use_year=2000)
     edu_pars = dict(init_dropout=0.2)
-    fp_pars = dict(postpartum_dur=24)
+    fp_pars = dict(primary_infertility=0.1)
     sim1 = fp.Sim(sim_pars=par_kwargs, fp_pars=fp_pars, contra_pars=contra_pars, edu_pars=edu_pars, location='kenya')
     sim1.init()
 
     assert sim1.pars.n_agents == 500, "Sim par failed"
     assert sim1.connectors.contraception.pars.prob_use_year == contra_pars['prob_use_year'], "Contraception par failed"
     assert sim1.connectors.edu.pars.init_dropout.pars.p == edu_pars['init_dropout'], "Education par failed"
-    assert sim1.pars.fp.postpartum_dur == fp_pars['postpartum_dur'], "FP par failed"
+    assert sim1.pars.fp.primary_infertility == fp_pars['primary_infertility'], "FP par failed"
 
     # Test 2: separate modules
     contra_mod = fp.SimpleChoice(location='kenya', prob_use_trend_par=0.3)
     edu_mod = fp.Education(location='kenya', init_dropout=0.1)
 
-    sim2 = fp.Sim(pars=par_kwargs, postpartum_dur=21, contraception_module=contra_mod, education_module=edu_mod, location='kenya')
+    sim2 = fp.Sim(pars=par_kwargs, primary_infertility=0.1, contraception_module=contra_mod, education_module=edu_mod, location='kenya')
     sim2.init()
 
     assert sim2.connectors.contraception.pars.prob_use_trend_par == 0.3, "Contraception par failed"
     assert sim2.connectors.edu.pars.init_dropout.pars.p == 0.1, "Education par failed"
-    assert sim2.pars.fp.postpartum_dur == 21, "FP par failed"
+    assert sim2.pars.fp.primary_infertility == 0.1, "FP par failed"
 
     # Test 3: flat pars dict
     pars = dict(
         start=2010,  # Sim par
-        postpartum_dur=18,  # FP par
+        primary_infertility=0.18,  # FP par
         prob_use_intercept=0.5,  # Contraception par
         init_dropout=0.15,  # Education par
         location='kenya',
@@ -102,7 +102,7 @@ def test_sim_creation():
 
     assert sim3.connectors.contraception.pars.prob_use_intercept == 0.5, "Contraception par failed"
     assert sim3.connectors.edu.pars.init_dropout.pars.p == 0.15, "Education par failed"
-    assert sim3.pars.fp.postpartum_dur == 18, "FP par failed"
+    assert sim3.pars.fp.primary_infertility == 0.18, "FP par failed"
 
     # Test 4: mixed types: some flat pars, some in dicts, and some modules
     fp_pars = dict(short_int=20)
@@ -115,7 +115,7 @@ def test_sim_creation():
     assert sim4.connectors.contraception.pars.prob_use_year == 2010, "Contraception par failed"
     assert sim4.connectors.edu.pars.age_start == 7, "Education par failed"
     assert sim4.pars.fp.short_int == 20, "FP par failed"
-    assert sim4.pars.fp.postpartum_dur == 18, "FP par failed"
+    assert sim4.pars.fp.primary_infertility == 0.18, "FP par failed"
 
     print('✓ (successfully created sims with different methods)')
 
@@ -140,8 +140,8 @@ if __name__ == '__main__':
     # sim = test_simple('senegal')
     # s1 = test_random_choice()
     # sims1 = test_simple_choice()
-    sims2 = test_mid_choice()
-    # test_sim_creation()
+    # sims2 = test_mid_choice()
+    test_sim_creation()
     # exp = test_senegal()
 
     print('Done.')
