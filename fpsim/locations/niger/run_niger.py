242f46252b022dc46fd51012cf829cf9e40c6d48
lrkbruce <lauryn.keeler.bruce@gmail.com>
2025-09-04 16:00:14 -0700

    Created functioning run_niger.py and run_cotedivoire.py files. Changed save output folder to calib_results with a figures subdirectory.

--- fpsim/locations/niger/run_niger.py
+++ fpsim/locations/niger/run_niger.py
@@ -20,21 +20,19 @@ import fpsim as fp
 import pandas as pd
 import sciris as sc
 import starsim as ss
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 from fpsim import plotting as fpplt
-
+import os
 
 # Settings
 country = 'niger'
-fpplt.Config.set_figs_directory('figures/')
+fpplt.Config.set_figs_directory('calib_results/figures/')
 fpplt.Config.do_save = True
 fpplt.Config.do_show = False
-fpplt.Config.show_rmse = False
-
+fpplt.Config.show_rmse = True
 
 def make_pars():
     pars = fp.make_fp_pars()  # For default pars
-    print(pars)
     pars.update_location(country)
 
     # Modify individual fecundity and exposure parameters
@@ -54,14 +52,10 @@ def make_pars():
     # 'months': array([ 0.,  3.,  6.,  9., 12., 15., 18., 21., 24., 27., 30., 33., 36., 39., 42., 45., 48., 51., 54.]),
     # The probability of sex --> very indirect, so need a larger term, 
     # when you are 2 years postpartum, dhs data sexual activity, probability of sex
-    pars['spacing_pref'] = {}
-    pars['spacing_pref']['preference'] = [ 0.,  3.,  6.,  9., 12., 15., 18., 21., 24., 27., 30., 33., 36., 39., 42., 45., 48., 51., 54.]
-    pars['spacing_pref']['preference'] = np.array(pars['spacing_pref']['preference'])
     pars['spacing_pref']['preference'][:3] =  1  # Spacing of 0-6 months
-    pars['spacing_pref']['preference'][3:4] = 1  # Spacing of 9 months
-    pars['spacing_pref']['preference'][4:8] = 0.8  # Spacing of 12-24 months
-    pars['spacing_pref']['preference'][8:16] = 5  # Spacing of 24-48 months, e.g. 4x probabliity of sex
-    pars['spacing_pref']['preference'][16:] =  0.1  # Spacing of 48-56 months
+    pars['spacing_pref']['preference'][3:6] = 1  # Spacing of 9-15 months
+    pars['spacing_pref']['preference'][6:9] = 1  # Spacing of 18-24 months
+    pars['spacing_pref']['preference'][9:] =  1  # Spacing of 27-36 months
  
     return pars, cm_pars
 
@@ -72,73 +66,21 @@ def make_sim(pars=None, stop=2021):
 
     method_choice = fp.SimpleChoice(pars=cm_pars, location=country)  
 
-    # Run the sim
-    #sim = fp.Sim(
-    #    start=2000,
-    #    stop=stop,
-    #    n_agents=1000,
-    #    location=country,
-    #    pars=pars,
-    #    analyzers=[fp.cpr_by_age(), fp.method_mix_by_age()],
-    #    contraception_module = method_choice
-    #)
     sim = fp.Sim(pars=pars, contraception_module=method_choice)
 
     return sim
 
 
-def plot_cpr(sim, start_year=2005, end_year=None, ax=None, legend_kwargs={}):
-    '''
-    Plot contraceptive prevalence rate for model vs data
-    '''
-
-    # Import data
-    fpet = pd.read_csv('mcpr.csv')
-    res = sim.results
-
-    # Data to plot
-    plot_data = fpet.loc[fpet.year >= start_year]
-    si = sc.findfirst(res['timevec'] >= start_year)
-
-    # Plot
-    ax.plot(plot_data['year'], plot_data['50%']*100, label='FPET', color='black')
-    ax.fill_between(plot_data['year'], plot_data['2.5%']*100, plot_data['97.5%']*100, color='lightgray', label='FPET 95% CI')
-    ax.plot(res['timevec'].years[si:], res.contraception.mcpr[si:] * 100, label='FPsim', color='cornflowerblue')
-    ax.set_xlabel('Year')
-    ax.set_ylabel('Percent')
-    ax.set_title(f'mCPR')
-    ax.legend(**legend_kwargs)
-
-    return ax
-
 
 def plot_calib(sim, single_fig=False, fig_kwargs=None, legend_kwargs=None):
-    """ Plots the commonly used plots for calibration """
+    """ Plots the commonly used plots for calibration
+    Plotting class function which plots the primary calibration targets:
+    method mix, method use, cpr, total fertility rate, birth spacing, 
+    age at first birth, and age-specific fertility rate.
+    
+    """
 
-    if legend_kwargs is None:
-        legend_kwargs = {'frameon': False, 'loc': 'best', 'fontsize': 15}
-
-    if single_fig:
-        if fig_kwargs is None:
-            fig_kwargs = {'figsize': (15, 9)}
-        fig, axes = pl.subplots(2, 3, **fig_kwargs)
-        axes = axes.flatten()
-
-    def ax_arg(i):
-        """Returns the appropriate axis for plotting"""
-        return axes[i] if single_fig else None
-
-    plot_cpr(sim, ax=ax_arg(0), legend_kwargs=legend_kwargs)
-    fpplt.plot_tfr(sim, ax=ax_arg(1), legend_kwargs=legend_kwargs)
-    fpplt.plot_method_use(sim, ax=ax_arg(2), legend_kwargs=legend_kwargs)
-    fpplt.plot_method_mix(sim, ax=ax_arg(3), legend_kwargs=legend_kwargs)
-    fpplt.plot_afb(sim, ax=ax_arg(4), legend_kwargs=legend_kwargs)
-    fpplt.plot_birth_spacing(sim, ax=ax_arg(5), legend_kwargs=legend_kwargs)
-
-    if single_fig:
-        fig.tight_layout()
-        fig_name = 'figures/calibration_plots.png'
-        sc.savefig(fig_name, dpi=100)
+    fpplt.plot_calib(sim)
 
     return
 
@@ -149,11 +91,15 @@ if __name__ == '__main__':
         # Create simulation with parameters
         sim = make_sim()
         sim.run()
-    #    sc.saveobj(f'results/{country}_calib.sim', sim)
-    #else:
-    #    sim = sc.loadobj(f'results/{country}_calib.sim')
+
+        # Create directory in current directory if it doesn't exist
+        os.makedirs('calib_results', exist_ok=True)
+        sc.saveobj(f'calib_results/{country}_calib.sim', sim)
+    else:
+        os.makedirs('calib_results', exist_ok=True)
+        sim = sc.loadobj(f'calib_results/{country}_calib.sim')
 
     # Set options for plotting
-    # sc.options(fontsize=20)  # Set fontsize
-    #plot_calib(sim, single_fig=True)
+    sc.options(fontsize=20)  # Set fontsize
+    plot_calib(sim, single_fig=True)
 
