43a787390784270a48d6a96917cb67b4fe93a7a8
Robyn Stuart <robyna.s@gmail.com>
2025-09-11 14:00:05 +1000

    move up

--- fpsim/locations/ethiopia/regions/snnpr.py
+++ fpsim/locations/ethiopia/regions/snnpr.py
@@ -13,17 +13,10 @@ import fpsim.locations.data_utils as fpld
 def make_calib_pars():
     pars = {}
     pars['fecundity'] = ss.uniform(0.945, 1.15)
-
-    exposure_correction_age = np.array([[0, 5, 10, 12.5, 15, 18, 20, 25, 30, 35, 40, 45, 50],
+    pars['exposure_age'] = np.array([[0, 5, 10, 12.5, 15, 18, 20, 25, 30, 35, 40, 45, 50],
                                         [1, 1, 1, 1, 1, 1, 1, 0.8, 0.8, 0.8, 1, 1, 1]])
-    exposure_age_interp = fp.data2interp(exposure_correction_age, fpd.spline_preg_ages)
-    pars['exposure_age'] = exposure_age_interp
-
-    exposure_correction_parity = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],
+    pars['exposure_parity'] = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],
                                            [1, 1, 1, 1, 1, 1, 1, 0.8, 0.5, 0.3, 0.15, 0.10, 0.05, 0.01]])
-    exposure_parity_interp = fp.data2interp(exposure_correction_parity, fpd.spline_parities)
-
-    pars['exposure_parity'] = exposure_parity_interp
     return pars
 
 
--- fpsim/locations/kenya/kenya.py
+++ fpsim/locations/kenya/kenya.py
@@ -12,18 +12,15 @@ def make_calib_pars():
     pars = {}
 
     # FP parameters - move to csv?
-    exposure_age = np.array([
+    pars['exposure_age'] = np.array([
                 [0, 5, 10, 12.5, 15, 18, 20, 25, 30, 35, 40, 45, 50],
                 [1, 1, 1,  1 ,   .4, 1.3, 1.5 ,.8, .8, .5, .3, .5, .5]
     ])
-    pars['exposure_age'] = fp.data2interp(exposure_age, fpd.spline_preg_ages)
 
-    # move to csv?
-    exposure_correction_parity = np.array([
+    pars['exposure_parity'] = np.array([
                 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],
                 [1, 1, 1, 1, 1, 1, 1, 0.8, 0.5, 0.3, 0.15, 0.10, 0.05, 0.01]
     ])
-    pars['exposure_parity'] = fp.data2interp(exposure_correction_parity, fpd.spline_parities)
 
     return pars
 
