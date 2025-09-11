2fb797f651c6000a572e9d77ff4be33a4664f3c2
lrkbruce <lauryn.keeler.bruce@gmail.com>
2025-09-11 11:40:45 -0700

    Added Nigeria Lagos region data and calibrated run_nigerialagos.py script and calibration figure outputs

---
+++ fpsim/locations/nigeria_lagos/run_nigerialagos.py
@@ -0,0 +1,105 @@
+"""
+Script to create a calibrated model of Niger.
+Run the model and generate plots showing the
+discrepancies between the model and data.
+"""
+import sys
+sys.path.insert(0, '/Users/')
+sys.path.insert(0, '/Users/laurynbruce/')
+sys.path.insert(0, '/Users/laurynbruce/')
+sys.path.insert(0, '/Users/laurynbruce/Documents/')
+sys.path.insert(0, '/Users/laurynbruce/Documents/Lauryn_Bruce/UCSD/')
+sys.path.insert(0, '/Users/laurynbruce/Documents/Lauryn_Bruce/UCSD/research/')
+sys.path.insert(0, '/Users/laurynbruce/Documents/Lauryn_Bruce/UCSD/research/Gates/')
+sys.path.insert(0, '/Users/laurynbruce/Documents/Lauryn_Bruce/UCSD/research/Gates/project/')
+sys.path.insert(0, '/Users/laurynbruce/Documents/Lauryn_Bruce/UCSD/research/Gates/project/fpsim/')
+sys.path.insert(0, '/Users/laurynbruce/Documents/Lauryn_Bruce/UCSD/research/Gates/project/fpsim/fpsim/')
+
+import numpy as np
+import fpsim as fp
+import pandas as pd
+import sciris as sc
+import starsim as ss
+import matplotlib.pyplot as plt
+from fpsim import plotting as fpplt
+import os
+
+# Settings
+country = 'nigeria_lagos'
+fpplt.Config.set_figs_directory('calib_results/figures/')
+fpplt.Config.do_save = True
+fpplt.Config.do_show = False
+fpplt.Config.show_rmse = True
+
+def make_pars():
+    pars = fp.make_fp_pars()  # For default pars
+    pars.update_location(country)
+
+    # Modify individual fecundity and exposure parameters
+    # These adjust each woman's probability of conception and exposure to pregnancy.
+    pars['fecundity_var_low'] = 2.2
+    pars['fecundity_var_high'] = 0.7
+    pars['exposure_factor'] = 0.5
+    
+    # Adjust contraceptive choice parameters
+    cm_pars = dict(prob_use_year = 2020,  # Base year
+                   prob_use_trend_par = 0.05,  # Time trend in contraceptive use - adjust this to get steeper/slower trend
+                   prob_use_intercept = -2.1,  # Intercept for the probability of using contraception - shifts the mCPR level
+                   method_weights = np.array([1.9, 3, 6, 0.08, 0.1, 0.001, 3, 0.03, 0.7]))
+
+    # Postpartum sexual activity correction or 'birth spacing preference'. Pulls values from {location}/data/birth_spacing_pref.csv by default
+    # Set all to 1 to reset. Option to use 'optimize-space-prefs.py' script in this directory to determine values
+    # 'months': array([ 0.,  3.,  6.,  9., 12., 15., 18., 21., 24., 27., 30., 33., 36., 39., 42., 45., 48., 51., 54.]),
+    # The probability of sex --> very indirect, so need a larger term, 
+    # when you are 2 years postpartum, dhs data sexual activity, probability of sex
+    pars['spacing_pref']['preference'][:3] =  1  # Spacing of 0-6 months
+    pars['spacing_pref']['preference'][3:6] = 1  # Spacing of 9-15 months
+    pars['spacing_pref']['preference'][6:9] = 0.6  # Spacing of 18-24 months
+    pars['spacing_pref']['preference'][9:] =  0.5  # Spacing of 27-54 months
+ 
+    return pars, cm_pars
+
+
+def make_sim(pars=None, stop=2021):
+    if pars is None:
+        pars, cm_pars = make_pars()
+
+    method_choice = fp.SimpleChoice(pars=cm_pars, location=country)  
+
+    sim = fp.Sim(pars=pars, contraception_module=method_choice)
+
+    return sim
+
+
+
+def plot_calib(sim, single_fig=False, fig_kwargs=None, legend_kwargs=None):
+    """ Plots the commonly used plots for calibration
+    Plotting class function which plots the primary calibration targets:
+    method mix, method use, cpr, total fertility rate, birth spacing, 
+    age at first birth, and age-specific fertility rate.
+    
+    """
+
+    fpplt.plot_calib(sim)
+
+    return
+
+
+if __name__ == '__main__':
+    do_run = True  # Whether to run the sim or load from file
+    if do_run:
+        # Create simulation with parameters
+        sim = make_sim()
+        sim.run()
+
+        # Create directory in current directory if it doesn't exist
+        os.makedirs('calib_results', exist_ok=True)
+        sc.saveobj(f'calib_results/{country}_calib.sim', sim)
+    else:
+        os.makedirs('calib_results', exist_ok=True)
+        sim = sc.loadobj(f'calib_results/{country}_calib.sim')
+
+    # Set options for plotting
+    sc.options(fontsize=20)  # Set fontsize
+    plot_calib(sim, single_fig=True)
+
