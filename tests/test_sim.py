52bd72476d766f2f84bf6296267dc03547e35c25
Robyn Stuart <robyna.s@gmail.com>
2025-09-11 20:52:17 +1000

    more

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -139,10 +139,10 @@ def test_senegal():
 if __name__ == '__main__':
 
     sim = test_simple()
-    s1 = test_random_choice()
-    sims1 = test_simple_choice()
-    sims2 = test_mid_choice()
-    test_sim_creation()
-    exp = test_senegal()
+    # s1 = test_random_choice()
+    # sims1 = test_simple_choice()
+    # sims2 = test_mid_choice()
+    # test_sim_creation()
+    # exp = test_senegal()
 
     print('Done.')
