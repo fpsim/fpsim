7134755510d4595987c78f0b3f3cf899316e841a
Robyn Stuart <robyna.s@gmail.com>
2025-09-11 14:00:05 +1000

    move up

--- fpsim/locations/senegal/senegal.py
+++ fpsim/locations/senegal/senegal.py
@@ -10,15 +10,10 @@ import fpsim.locations.data_utils as fpld
 def make_calib_pars():
     """ Make a dictionary of location-specific parameters """
     pars = {}
-
-    exposure_age = np.array([[0, 5, 10, 12.5, 15, 18, 20, 25, 30, 35, 40, 45, 50],
+    pars['exposure_age'] = np.array([[0, 5, 10, 12.5, 15, 18, 20, 25, 30, 35, 40, 45, 50],
                                         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
-    pars['exposure_age'] = fp.data2interp(exposure_age, fpd.spline_preg_ages)
-
-    exposure_correction_parity = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],
+    pars['exposure_parity'] = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],
                                            [1, 1, 1, 1, 1, 1, 1, 0.8, 0.5, 0.3, 0.15, 0.10, 0.05, 0.01]])
-    pars['exposure_parity'] = fp.data2interp(exposure_correction_parity, fpd.spline_parities)
-
     return pars
 
 
